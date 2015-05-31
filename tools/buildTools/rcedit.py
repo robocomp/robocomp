79129ef89384c66e72cc89a6c1dc8f41c7f4658f
nithin murali <imnmfotmal@gmail.com>
2015-05-31 21:59:26 +0530

    added basic rcedit

---
+++ tools/buildTools/rcedit.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+from __future__ import print_function
+import argparse
+import os
+import string
+import workspace
+
+
+''' edit files of robocomp compoent '''
+
+def main():
+    parser = argparse.ArgumentParser(description="initialize a robocomp workspace")
+    parser.add_argument('component', nargs=1, help='name of the component')
+    parser.add_argument('file', nargs=1, help='name of the file of the given component you need to edit')
+    args = parser.parse_args()
+
+    sfile = args.file[0]
+    component = args.component[0]
+
+    filelist = workspace.rc_ws.search_for_file(component,[str(sfile)])
+    if len(filelist) == 0:
+        parser.error("couldnt find the file {0} in {1}".format(sfile,component))
+    
+    #get editor command
+    editor = 'vim'
+
+    if len(filelist) == 1:
+        command = editor +  " " + filelist[0][1]
+    else:
+        print("This is a non-unique file, please one of the following:\n")
+        count=1
+        for fpath in filelist:
+            print(count+') '+fpath[1])
+            count += 1
+        option = input("please selact a file : ")
+        command = editor + " " + filelist[option][1]
+
+    try:
+        #print(command)
+        os.system(command)
+    except Exception as openEx:
+        print("Cant open the specifiled file : %s" % (str(openEx)) )
+
+
+if __name__ == '__main__':
+    main()
