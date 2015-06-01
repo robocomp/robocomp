7f21c3679cf739e295711f7e3283bae4cbc7ea01
nithin murali <imnmfotmal@gmail.com>
2015-06-01 22:27:31 +0530

    bug fixes in toplevel

--- cmake/toplevel.cmake
+++ cmake/toplevel.cmake
@@ -7,12 +7,26 @@ if( NOT EXISTS "../.rc_workspace")
     message(FATAL_ERROR "This is not a Robocomp workspace")
 endif()
 
+MACRO(SUBDIRLIST result curdir)
+  FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
+  SET(dirlist "")
+  FOREACH(child ${children})
+    IF(IS_DIRECTORY ${curdir}/${child})
+        LIST(APPEND dirlist ${child})
+    ENDIF()
+  ENDFOREACH()
+  SET(${result} ${dirlist})
+ENDMACRO()
+
 #ADD the components
+SUBDIRLIST(SUBDIRS ${CMAKE_CURRENT_SOURCE_DIR})
+string(ASCII 27 Esc)
+set(ColourReset "${Esc}[m")
+set(Red         "${Esc}[31m")
+
 FOREACH(SUBDIR ${SUBDIRS})
+    message(STATUS "${Red}Configuring Component ${SUBDIR} ${ColourReset}")
     IF(NOT EXISTS "${SUBDIR}/.ignore_comp")
         ADD_SUBDIRECTORY(${SUBDIR})
-        message(STATUS "ADDING COMPONENT ${SUBDIR}")
     ENDIF()
 ENDFOREACH()
-
-#for the time being we are not shifting executables into devel
