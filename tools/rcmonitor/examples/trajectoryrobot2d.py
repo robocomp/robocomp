07b4be5b31fd68700244c7815ea3db80510bbcf7
mhaut <juanmariohaut@unex.es>
2015-07-17 20:51:57 +0200

    add multitargets

--- tools/rcmonitor/examples/trajectoryrobot2d.py
+++ tools/rcmonitor/examples/trajectoryrobot2d.py
@@ -23,6 +23,7 @@ import Ice, sys, math, traceback
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.Qt import *
+from random import uniform
 
 class C(QWidget):
 	def __init__(self, endpoint, modules):
@@ -66,10 +67,84 @@ class C(QWidget):
 		self.steer.move(50,58)
 		self.steer.show()
 
+
+		self.positionRangeXLabel = QLabel("Range X", self)
+		self.positionRangeXLabel.show()
+		self.positionRangeXLabel.move(205,8)
+		self.positionRangeXmin = QSpinBox(self)
+		self.positionRangeXmin.setMaximum(10000)
+		self.positionRangeXmin.setMinimum(-10000)
+		self.positionRangeXmin.show()
+		self.positionRangeXmin.setValue(0)
+		self.positionRangeXmin.setSingleStep(25)
+		self.positionRangeXmin.move(255,8)
+		self.positionRangeXmax = QSpinBox(self)
+		self.positionRangeXmax.setMaximum(10000)
+		self.positionRangeXmax.setMinimum(-10000)
+		self.positionRangeXmax.show()
+		self.positionRangeXmax.setValue(0)
+		self.positionRangeXmax.setSingleStep(25)
+		self.positionRangeXmax.move(335,8)
+
+
+		self.positionRangeZLabel = QLabel("Range Z", self)
+		self.positionRangeZLabel.show()
+		self.positionRangeZLabel.move(205,33)
+		self.positionRangeZmin = QSpinBox(self)
+		self.positionRangeZmin.setMaximum(10000)
+		self.positionRangeZmin.setMinimum(-10000)
+		self.positionRangeZmin.show()
+		self.positionRangeZmin.setValue(0)
+		self.positionRangeZmin.setSingleStep(25)
+		self.positionRangeZmin.move(255,33)
+		self.positionRangeZmax = QSpinBox(self)
+		self.positionRangeZmax.setMaximum(10000)
+		self.positionRangeZmax.setMinimum(-10000)
+		self.positionRangeZmax.show()
+		self.positionRangeZmax.setValue(0)
+		self.positionRangeZmax.setSingleStep(25)
+		self.positionRangeZmax.move(335,33)
+
+		self.steerRangeLabel = QLabel('R.Angle', self)
+		self.steerRangeLabel.show()
+		self.steerRangeLabel.move(205,58)
+		self.steerRangemin = QDoubleSpinBox(self)
+		self.steerRangemin.setMaximum(3.14*10)
+		self.steerRangemin.setMinimum(-3.14*10)
+		self.steerRangemin.setSingleStep(0.05)
+		self.steerRangemin.setValue(0)
+		self.steerRangemin.move(255,58)
+		self.steerRangemin.show()
+		
+		self.steerRangemax = QDoubleSpinBox(self)
+		self.steerRangemax.setMaximum(3.14*10)
+		self.steerRangemax.setMinimum(-3.14*10)
+		self.steerRangemax.setSingleStep(0.05)
+		self.steerRangemax.setValue(0)
+		self.steerRangemax.move(335,58)
+		self.steerRangemax.show()
+		w
+		self.numTargetsLabel = QLabel('Targets', self)
+		self.numTargetsLabel.show()
+		self.numTargetsLabel.move(205,80)
+		self.numTargets = QSpinBox(self)
+		self.numTargets.setMaximum(10000)
+		self.numTargets.setMinimum(-10000)
+		self.numTargets.show()
+		self.numTargets.setValue(0)
+		self.numTargets.setSingleStep(25)
+		self.numTargets.move(255,80)
+
 		self.resetButton = QPushButton("go", self)
 		self.resetButton.move(5,90)
 		self.resetButton.show()
 		self.connect(self.resetButton, SIGNAL('clicked()'), self.go)
+		self.show()
+
+		self.resetButton = QPushButton("goAll", self)
+		self.resetButton.move(355,90)
+		self.resetButton.show()
+		self.connect(self.resetButton, SIGNAL('clicked()'), self.goAll)
 		self.show()
 		
 	def job(self):
@@ -89,3 +164,30 @@ class C(QWidget):
 		self.proxy.go(tp)
 		print 'a2'
 
+	def goAll(self):
+		print 'a1'
+		tp = self.mods['RoboCompTrajectoryRobot2D'].TargetPose()
+
+		minRangeX = self.positionRangeXmin.value()
+		maxRangeX = self.positionRangeXmax.value()
+		minRangeZ = self.positionRangeZmin.value()
+		maxRangeZ = self.positionRangeZmax.value()
+		minRangeSteer = self.steerRangemin.value()
+		maxRangeSteer = self.steerRangemax.value()
+		numTargets = self.numTargets.value()
+
+		while numTargets > 0:			
+			tp.x = uniform(minRangeX,maxRangeX)
+			tp.z = uniform(minRangeZ,maxRangeZ)
+			tp.y = 0
+			tp.rx = 0
+			tp.ry = uniform(minRangeSteer,maxRangeSteer)
+			tp.rz = 0
+			self.proxy.go(tp)
+			while True:
+				state = self.proxy.getState()
+				state = state.state
+				if state == "IDLE":
+					print "in target number: "+str(self.numTargets.value() - numTargets)
+					numTargets -= 1
+					break
