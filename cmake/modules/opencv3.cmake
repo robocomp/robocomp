88effca675251b4b53e8d56d46c4de6c9530dccb
Marco A. Guiterrez <marcog@unex.es>
2016-04-12 13:17:15 +0800

    added opencv3 module

---
+++ cmake/modules/opencv3.cmake
@@ -0,0 +1,7 @@
+FIND_PACKAGE( OpenCV 3 REQUIRED )
+INCLUDE_DIRECTORIES( ${OpenCV_INCLUDE_DIR} )
+SET(LIBS ${LIBS} ${OpenCV_LIBS})
+MESSAGE(STATUS ${OpenCV_LIBS})
+
+# INCLUDE_DIRECTORIES( /usr/local/include/opencv /usr/include/opencv )
+# SET( LIBS ${LIBS} -L/usr/local/lib -L/usr/lib -lcv -lhighgui -lcvaux -lml -lcxcore )
