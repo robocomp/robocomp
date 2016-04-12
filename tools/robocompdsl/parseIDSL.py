9967ab369b36ad3086a14a71e43ecbdb0ff30e93
Daniel1108 <dgallegos@alumnos.unex.es>
2016-04-13 00:44:55 +0200

    RosPublisher done

--- tools/robocompdsl/parseIDSL.py
+++ tools/robocompdsl/parseIDSL.py
@@ -10,10 +10,25 @@ import sys, traceback, os
 
 class IDSLParsing:
 	@staticmethod
+	def fromFileIDSL(filename):
+		inputText = open(filename, 'r').read()
+		try:
+			ret = IDSLParsing.fromString(inputText)
+		except ParseException, p:
+			print 'Error reading IDSL', filename
+			traceback.print_exc()
+			print 'Error reading IDSL', filename
+			print p.markInputline()
+			os._exit(1)
+		ret['filename'] = filename
+		return ret
+	
+	@staticmethod
 	def fromFile(filename):
 		inputText = open(filename, 'r').read()
 		try:
 			ret = IDSLParsing.fromString(inputText)
+			ret = IDSLParsing.module(ret)
 		except ParseException, p:
 			print 'Error reading IDSL', filename
 			traceback.print_exc()
@@ -69,19 +84,170 @@ class IDSLParsing:
 		tree = IDSL.parseString(text)
 		return tree
 
-if __name__ == '__main__':
-	sequencesList = []
-	idsl = IDSLParsing.fromFile(sys.argv[1])
-	for imp in idsl['module']['contents']:
-		if imp['type'] == 'sequence':
-			sequencesList.append(imp)
-		if imp['type'] == 'struct':
-			for structVar in imp['structIdentifiers']:
-				wasSequence = False
-				for sequences in sequencesList:
-					if structVar[0] == sequences['name']:
-						print sequences['typeSequence']
-						wasSequence = True
+	@staticmethod
+	def gimmeIDSL(name, files=''):
+		pathList = []
+		fileList = []
+		for p in [f for f in files.split('#') if len(f)>0]:
+			if p.startswith("-I"):
+				pathList.append(p[2:])
+			else:
+				fileList.append(p)
+		pathList.append('/home/robocomp/robocomp/interfaces/IDSLs/')
+		pathList.append('/opt/robocomp/interfaces/IDSLs/')
+		filename = name.split('.')[0]
+		for p in pathList:
+			try:
+				path = p+'/'+name
+				return IDSLParsing.fromFile(path)
+			except IOError, e:
+				pass
+		print 'Couldn\'t locate ', name
+		sys.exit(-1)
+		
+	@staticmethod
+	def module(tree, start=''):
+		module = {}
+
+		#module name
+		module['name'] = tree['module']['name']
+
+		module['imports'] = ''
+		if 'imports' in tree:
+			#print module['name'], tree['imports']
+			for imp in tree['imports']:
+				#print 'proc', imp
+				#print 'has', IDSLParsing.gimmeIDSL(imp)['imports']
+				#print ''
+				module['imports'] += imp + '#' + IDSLParsing.gimmeIDSL(imp)['imports']
+		# INTERFACES DEFINED IN THE MODULE
+		module['interfaces'] = []
+		for contentDef in tree['module']['contents']:
+			if contentDef[0] == 'interface':
+				interface = { 'name':contentDef[1], 'methods':{}}
+				for method in contentDef[2]:
+					interface['methods'][method['name']] = {}
+
+					interface['methods'][method['name']]['name'] = method['name']
+					try:
+						interface['methods'][method['name']]['decorator'] = method['decorator']
+					except:
+						interface['methods'][method['name']]['decorator'] = ''
+
+					interface['methods'][method['name']]['return'] = method['ret']
+
+					params = []
+					try:
+						for p in method['params']:
+							try:
+								params.append( { 'decorator':p['decorator'], 'type':p['type'], 'name':p['name'] } )
+							except:
+								params.append( { 'decorator':'none',         'type':p['type'], 'name':p['name'] } )
+					except:
+						pass
+					interface['methods'][method['name']]['params'] = params
+
+					try:
+						interface['methods'][method['name']]['throws'] = method['decorator']
+					except:
+						interface['methods'][method['name']]['throws'] = 'nothing'
+				module['interfaces'].append(interface)
+		# TYPES DEFINED IN THE MODULE
+		module['types'] = []
+		#print '---\n---\nPARSE IDSL TYPES'
+		for contentDef in tree['module']['contents']:
+			#print contentDef[0]
+			if contentDef[0] in [ 'enum', 'struct', 'exception' ]:
+				typedef = { 'name':contentDef[1], 'type':contentDef[0]}
+				#print typedef
+				module['types'].append(typedef)
+			elif contentDef[0] in [ 'sequence', 'dictionary' ]:
+				typedef = { 'name':contentDef[-1], 'type':contentDef[0]}
+				#print typedef
+				module['types'].append(typedef)
+			elif contentDef[0] in ['interface']:
+				pass
+			else:
+				print 'Unknown module content', contentDef
+		# STRUCTS DEFINED IN THE MODULE
+		module['structs'] = []
+		for contentDef in tree['module']['contents']:
+			if contentDef['type'] == 'struct':
+				structdef = { 'name':tree['module']['name']+"/"+contentDef['name'], 'type':contentDef['type']}
+				#print structdef
+				module['structs'].append(structdef)
+
+		return module
+
+	@staticmethod
+	def printModule(module, start=''):
+		print 'MODULE', module['name']+':'
+		print ' ', 'INTERFACES:'
+		for interface in module['interfaces']:
+			print '   ', interface['name']
+			for mname in interface['methods']:
+				method = interface['methods'][mname]
+				print '     ', method['name']
+				print '        decorator', method['decorator']
+				print '        return', method['return']
+				print '        params'
+				for p in method['params']:
+					print '         ', '<', p['decorator'], '>  <', p['type'], '>  <', p['name'], '>'
+
+
+
+
+
+
+
+class IDSLPool:
+	def __init__(self, files):
+		self.modulePool = {}
+		self.includeInPool(files, self.modulePool)
+	def includeInPool(self, files, modulePool):
+		pathList = []
+		fileList = []
+		for p in [f for f in files.split('#') if len(f)>0]:
+			if p.startswith("-I"):
+				pathList.append(p[2:])
+			else:
+				fileList.append(p)
+		pathList.append('/home/robocomp/robocomp/interfaces/IDSLs/')
+		pathList.append('/opt/robocomp/interfaces/IDSLs/')
+		for f in fileList:
+			filename = f.split('.')[0]
+			if not filename in modulePool:
+				for p in pathList:
+					try:
+						path = p+'/'+f
+						module = IDSLParsing.fromFile(path)
+						modulePool[filename] = module
+						#for importf in module['imports'].split('#'):
+							#print 'aqui', importf
+						self.includeInPool(module['imports'], modulePool)
 						break
-				if not wasSequence:
-					print "not wasSequence"
+					except IOError, e:
+						pass
+				if not filename in self.modulePool:
+					print 'Couldn\'t locate ', f
+					sys.exit(-1)
+
+	def moduleProviding(self, interface):
+		for module in self.modulePool:
+			for m in self.modulePool[module]['interfaces']:
+				if m['name'] == interface:
+					return self.modulePool[module]
+		return None
+	
+	def rosImports(self):
+		includesList = []
+		for module in self.modulePool:
+			for m in self.modulePool[module]['structs']:
+				includesList.append(m['name'])
+		return includesList
+	
+
+if __name__ == '__main__':
+	idsl = IDSLParsing.fromFile(sys.argv[1])
+	for imp in idsl['imports']:
+		print imp
