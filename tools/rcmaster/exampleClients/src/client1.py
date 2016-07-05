4f1bee3adfc7b91bf63ab4ee0df17927010fad12
yashsanap <yashsanap@gmail.com>
2016-07-05 11:03:30 +0530

    adding and example client

---
+++ tools/rcmaster/exampleClients/src/client1.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2016 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# \mainpage RoboComp::client1
+#
+# \section intro_sec Introduction
+#
+# Some information about the component...
+#
+# \section interface_sec Interface
+#
+# Descroption of the interface provided...
+#
+# \section install_sec Installation
+#
+# \subsection install1_ssec Software depencences
+# Software dependences....
+#
+# \subsection install2_ssec Compile and install
+# How to compile/install the component...
+#
+# \section guide_sec User guide
+#
+# \subsection config_ssec Configuration file
+#
+# <p>
+# The configuration file...
+# </p>
+#
+# \subsection execution_ssec Execution
+#
+# Just: "${PATH_TO_BINARY}/client1 --Ice.Config=${PATH_TO_CONFIG_FILE}"
+#
+# \subsection running_ssec Once running
+#
+#
+#
+
+import sys, traceback, Ice, IceStorm, subprocess, threading, time, Queue, os, copy
+
+# Ctrl+c handling
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+from PySide import *
+
+from specificworker import *
+
+ROBOCOMP = ''
+try:
+    ROBOCOMP = os.environ['ROBOCOMP']
+except:
+    print '$ROBOCOMP environment variable not set, using the default value /opt/robocomp'
+    ROBOCOMP = '/opt/robocomp'
+if len(ROBOCOMP)<1:
+    print 'ROBOCOMP environment variable not set! Exiting.'
+    sys.exit()
+
+
+preStr = "-I"+ROBOCOMP+"/interfaces/ -I/opt/robocomp/interfaces/ --all "+ROBOCOMP+"/interfaces/"
+Ice.loadSlice(preStr+"CommonBehavior.ice")
+import RoboCompCommonBehavior
+Ice.loadSlice(preStr+"RCMaster.ice")
+import RoboCompRCMaster
+Ice.loadSlice(preStr+"ASR.ice")
+import RoboCompASR
+
+
+class CommonBehaviorI(RoboCompCommonBehavior.CommonBehavior):
+    def __init__(self, _handler, _communicator):
+        self.handler = _handler
+        self.communicator = _communicator
+    def getFreq(self, current = None):
+        self.handler.getFreq()
+    def setFreq(self, freq, current = None):
+        self.handler.setFreq()
+    def timeAwake(self, current = None):
+        try:
+            return self.handler.timeAwake()
+        except:
+            print 'Problem getting timeAwake'
+    def killYourSelf(self, current = None):
+        self.handler.killYourSelf()
+    def getAttrList(self, current = None):
+        try:
+            return self.handler.getAttrList(self.communicator)
+        except:
+            print 'Problem getting getAttrList'
+            traceback.print_exc()
+            status = 1
+            return
+
+
+
+if __name__ == '__main__':
+    app = QtCore.QCoreApplication(sys.argv)
+    params = copy.deepcopy(sys.argv)
+    if len(params) > 1:
+        if not params[1].startswith('--Ice.Config='):
+            params[1] = '--Ice.Config=' + params[1]
+    elif len(params) == 1:
+        params.append('--Ice.Config=config')
+    ic = Ice.initialize(params)
+    status = 0
+    mprx = {}
+    try:
+
+        # Remote object connection for rcmaster
+        try:
+            proxyString = ic.getProperties().getProperty('rcmasterProxy')
+            try:
+                with open(os.path.join(os.path.expanduser('~'), ".config/RoboComp/rcmaster.config"), 'r') as f:
+                    rcmaster_uri = f.readline().strip().split(":")
+                print "connecting to rcmaster " ,rcmaster_uri
+                basePrx = ic.stringToProxy("rcmaster:tcp -h "+rcmaster_uri[0]+" -p "+rcmaster_uri[1])
+                try:
+                    rcmaster_proxy = RoboCompRCMaster.rcmasterPrx.checkedCast(basePrx)
+                except Ice.ConnectionRefusedException:
+                    raise Exception("RCMaster in not running")
+                
+                compInfo = RoboCompRCMaster.compData()
+                compInfo.name = "client1"
+                interface = RoboCompRCMaster.interfaceData()
+                interface.name = 'asr'
+                compInfo.interfaces = [interface]
+                print compInfo
+                idata = rcmaster_proxy.registerComp(compInfo,False,True)
+                print idata
+                mprx["rcmasterProxy"] = rcmaster_proxy
+            except Ice.Exception:
+                print 'Cannot connect to the remote object (rcmaster)'
+                traceback.print_exc()
+                status = 1
+        except Ice.Exception, e:
+            print e
+            print 'Cannot get rcmasterProxy property.'
+            status = 1
+
+    except:
+            traceback.print_exc()
+            status = 1
+
+
+    if status == 0:
+        worker = SpecificWorker(mprx)
+
+
+        adapter = ic.createObjectAdapterWithEndpoints('ASR',idata[0].protocol+' -h localhost -p '+str(idata[0].port))
+        adapter.add(ASRI(worker), ic.stringToIdentity('asr'))
+        adapter.activate()
+
+
+#       adapter.add(CommonBehaviorI(<LOWER>I, ic), ic.stringToIdentity('commonbehavior'))
+
+        app.exec_()
+
+    if ic:
+        try:
+            ic.destroy()
+        except:
+            traceback.print_exc()
+            status = 1
