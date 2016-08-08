db2b15a9c7a12bb651160a0171ea24bc5100403f
yashsanap <yashsanap@gmail.com>
2016-08-09 02:27:14 +0530

    adding test dir

---
+++ tools/rcmaster/test/test.py
@@ -0,0 +1,17 @@
+class CompHandler():
+	'''
+
+	'''
+	def __init__(self, path):
+		pass
+
+	def start_comp(self, Name, path=''):
+		pass
+
+	def kill_comp(self, Name):
+		pass
+
+
+if __name__ == '__main__':
+
+	
