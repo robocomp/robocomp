66d2ff3046612f03905d0317b63ec1ea2904f30f
H20 <basilmvarghese70@gmail.com>
2016-08-23 20:17:00 +0530

    Added the changes made in the Basil_GSOC to Basil_GSOC_2016 branch.It is because Basil_GSOC was branched from master and Basil_GSOC_2016 from highlyunstable.I had to merge my branch into highlyunstable

---
+++ tools/rcmanager/CMakeFiles/ui_editorForm.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ui_editorForm"
+  "ui_editorForm.py"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ui_editorForm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
