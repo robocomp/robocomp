731e5424b25d3b4b449d81e74fd0a182ef88a56f
Luis J. Manso <luis.manso@gmail.com>
2017-04-20 11:30:35 +0200

    Include access to InnerModelJoint in the python bindings

--- libs/innermodel/innermodel_python.cpp
+++ libs/innermodel/innermodel_python.cpp
@@ -35,6 +35,43 @@ BOOST_PYTHON_MODULE(librobocomp_innermodel)
     	 )
     	 .def("transform", transform)
     	 .def("getTransformationMatrix", &InnerModel::getTransformationMatrixS)
-    	 .def("getParentIdentifier", &InnerModel::getParentIdentifierS)
+	 .def("getParentIdentifier", &InnerModel::getParentIdentifierS)
+
+	 .def("getJoint", &InnerModel::getJointS, boost::python::return_value_policy<boost::python::manage_new_object>())
 	;
+
+
+	/*
+	class_<InnerModelJoint>("InnerModelJoint",
+	 init<
+	  QString,
+	  float, float, float,
+	  float, float, float,
+	  float, float, float,
+	  float, float, float,
+	  float, float,
+	  uint32_t, std::string, float, InnerModelTransform * >
+	 (
+	  arg("id_"),
+	  arg("lx_"), arg("ly_"), arg("lz_"),
+	  arg("hx_"), arg("hy_"), arg("hz_"),
+	  arg("tx_"), arg("ty_"), arg("tz_"),
+	  arg("rx_"), arg("ry_"), arg("rz_"),
+	  arg("min_") = -INFINITY, arg("max_") = INFINITY,
+	  arg("port_") = 0, arg("axis") = "z", arg("home_") = 0, arg("parent_") = NULL
+	 ))
+*/
+	class_<InnerModelJoint>("InnerModelJoint", init<>())
+	 .def("getAngle", &InnerModelJoint::getAngle)
+	 .def("unitaryAxis", &InnerModelJoint::unitaryAxis)
+    	 .def("setAngle", &InnerModelJoint::setAngle,
+    	     (
+    	         arg("angle"),
+    	         arg("force")=false
+    	     )
+    	 )
+	;
+
+
+
 }
--- libs/innermodel/innermodeljoint.cpp
+++ libs/innermodel/innermodeljoint.cpp
@@ -1,21 +1,25 @@
 /*
  * Copyright 2016 <copyright holder> <email>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 #include "innermodeljoint.h"
+InnerModelJoint::InnerModelJoint() : InnerModelTransform("invalid",QString("static"), 0,0,0, 0,0,0, 0, NULL)
+{
+		throw std::string("Can't actually build InnerModelJoint using the default constructor");
+}
 
 InnerModelJoint::InnerModelJoint(QString id_, float lx_, float ly_, float lz_, float hx_, float hy_, float hz_, float tx_, float ty_, float tz_, float rx_, float ry_, float rz_, float min_, float max_, uint32_t port_, std::string axis_, float home_, InnerModelTransform *parent_) : InnerModelTransform(id_,QString("static"),tx_,ty_,tz_,rx_,ry_,rz_, 0, parent_)
 {
@@ -74,14 +78,14 @@ void InnerModelJoint::save(QTextStream &out, int tabs)
 	QMutexLocker l(mutex);
 	QList<InnerModelNode*>::iterator c;
 	//<joint id="head_yaw_joint" port="10067" axis="z" home="0" min="-1" max="1">
-	for (int i=0; i<tabs; i++) out << "\t";	
+	for (int i=0; i<tabs; i++) out << "\t";
 	out << "<joint id=\"" << id << "\" port=\"" << port << "\" axis=\"" <<QString::fromStdString( axis)<<"\" home=\""<< QString::number(home, 'g', 10)
 	<<"\" min=\""<< QString::number(min, 'g', 10)<<"\" max=\""<< QString::number(max, 'g', 10)
-	<< "\" tx=\""<< QString::number(backtX, 'g', 10) <<"\" ty=\""<< QString::number(backtY, 'g', 10) <<"\" tz=\""<< QString::number(backtZ, 'g', 10) 
+	<< "\" tx=\""<< QString::number(backtX, 'g', 10) <<"\" ty=\""<< QString::number(backtY, 'g', 10) <<"\" tz=\""<< QString::number(backtZ, 'g', 10)
 	<<"\"  rx=\""<< QString::number(backrX, 'g', 10) <<"\" ry=\""<< QString::number(backrY, 'g', 10) <<"\" rz=\""<< QString::number(backrZ, 'g', 10) <<"\">\n";
 	for (c=children.begin(); c!=children.end(); c++)
 			(*c)->save(out, tabs+1);
-	
+
 	for (int i=0; i<tabs; i++) out << "\t";
 	out << "</joint>\n";
 }
