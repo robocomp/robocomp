33f43901843da13a633eab61e72393c8a908e890
Mayank Kumar <kr.mayank1997@gmail.com>
2017-07-13 02:43:11 +0530

    Update main.py

--- tools/rcmanager/main.py
+++ tools/rcmanager/main.py
@@ -7,7 +7,6 @@ from viewer import Viewer
 from model import Model
 from controller import Controller
 from logger import RCManagerLogger
-
 from PyQt4 import QtCore, QtGui
 from rcmanagerSignals import rcmanager_signals
 
@@ -16,18 +15,19 @@ class Main():
     Viewer and the Controller, for our MVC model."""
 
     def __init__(self):
-    	signalObject = rcmanager_signals()
         xmldata = xml_reader(sys.argv[1])
         
+        self.signalObject = rcmanager_signals()
+        
         # create model as a NetworkX graph using dict
-        self.model = Model(xmldata, signalObject)
+        self.model = Model(xmldata, self.signalObject)
         
         # create Qt Ui in a separate class
-        # self.viewer = Viewer()
-        # self.viewer.show()
+        self.viewer = Viewer()
+        self.viewer.show()
         
         # create controller
-        controller = Controller(self.model, signalObject)
+        controller = Controller(self.model, self.viewer, self.signalObject)
         
         # calling the function to emit a signal
         self.model.sample_emit()
