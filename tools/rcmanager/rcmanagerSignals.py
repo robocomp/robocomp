7d3288ad0de6467971db9f7c04b2adeea5913f81
Mayank Kumar <kr.mayank1997@gmail.com>
2017-07-14 02:44:38 +0530

    Update rcmanagerSignals.py

--- tools/rcmanager/rcmanagerSignals.py
+++ tools/rcmanager/rcmanagerSignals.py
@@ -1,9 +1,11 @@
 
 from PyQt4 import QtCore
 
-class rcmanager_signals(QtCore.QObject):
+class rcmanagerSignals(QtCore.QObject):
 
-   	sample = QtCore.pyqtSignal()
+	sample = QtCore.pyqtSignal()
+   	init = QtCore.pyqtSignal(str)
+   	addNode = QtCore.pyqtSignal(dict)
 
 	def __init__(self, parent=None):
 		QtCore.QObject.__init__(self)
