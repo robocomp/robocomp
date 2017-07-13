44439a74b9fa3efef4569c806504012fc042d424
Mayank Kumar <kr.mayank1997@gmail.com>
2017-07-14 02:47:31 +0530

    Update controller.py

--- tools/rcmanager/controller.py
+++ tools/rcmanager/controller.py
@@ -2,32 +2,50 @@
 from logger import RCManagerLogger
 from PyQt4 import QtCore
 from PyQt4.QtCore import QObject, pyqtSignal, pyqtSlot
+import pdb
 
 class Controller():
     """This is the Controller object for our MVC model. It connects the Model
     and the Viewer, by reacting to the signals emitted by the Viewer and
     making the necessary changes to the Model"""
     
-    def __init__(self, model, viewer, rcmanager_signals):
-        self._logger = RCManagerLogger().get_logger("RCManager.Controller")
+    def __init__(self, rcmanagerSignals):
+        # self._logger = RCManagerLogger().get_logger("RCManager.Controller")
         # self._logger.info("Hello, this is Controller coming up")
-        # we set here the signal/slots connections to let data flow
 
         self.need_to_save = False
-        self.view = viewer
-        self.model = model
-        self.rcmanager_signals = rcmanager_signals
+        self.view = None
+        self.model = None
+        self.rcmanagerSignals = rcmanagerSignals
         
-        # reacting to a specific signal 
-        self.rcmanager_signals.sample.connect(self.sample_action)
+        self.isModelReady = False
+        self.isViewerReady = False
+        self.isControllerReady = False
+        
+       	self.signal_connections()
+       	
         pass
 
     def signal_connections(self):
-        pass
-        
-    def sample_action(self):
-		print "hello, this is printed because a sample signal was emitted"
-
+    	self.rcmanagerSignals.init.connect(self.init_action)
+    	
+    def init_action(self, string):
+    	print string, "object initialized"
+    	
+    	if string == 'Model':
+    		self.isModelReady = True
+    	elif string == 'Viewer':
+     		self.isViewerReady = True
+     	elif string == 'Controller':
+     		self.isControllerReady = True
+     		self.refresh_graph_from_model()
+     	
+    def refresh_graph_from_model(self):
+		# adding nodes
+		for node, data in self.model.graph.nodes_iter(data=True):
+			print "The controller sent signal to draw component:", data['@alias']
+			self.rcmanagerSignals.addNode.emit(data)
+	
     def load_manager_file(self, terminalArg=False, UserHaveChoice=True):  # To open the xml files ::Unfinished
         try:
             if self.need_to_save:  # To make sure the data we have been working on have been saved
