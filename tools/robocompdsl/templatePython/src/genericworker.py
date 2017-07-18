504362d499c71379a95a3ff1306eae38201e623f
nikhilsteph <nikhil.steph@gmail.com>
2017-07-19 00:12:26 +0530

    almost completed python generation

--- tools/robocompdsl/templatePython/src/genericworker.py
+++ tools/robocompdsl/templatePython/src/genericworker.py
@@ -57,6 +57,7 @@ Z()
 #    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys, Ice, os
+from Queue import Queue
 from PySide import *
 
 ROBOCOMP = ''
@@ -329,3 +330,54 @@ Z()
 		print "Period changed", p
 		Period = p
 		timer.start(Period)
+
+
+class CallQueue(object):
+	def __init__(self, maxsize=-1):
+		self.mutex = QtCore.QMutex()
+		self.call_buffer = Queue(maxsize=maxsize)
+		self.current_id = 0
+		self.result_buffer = dict()
+
+	def push(self, params):
+		"""
+		psuh a call to queue
+
+		:param params: params to call as a dictionary
+		"""
+		self.mutex.lock()
+		self.call_buffer.put_nowait(params)
+		self.current_id += 1
+		self.mutex.unlock()
+		print "new call added ", self.current_id
+		return self.current_id
+
+	def pop(self):
+		cid = self.current_id
+		self.mutex.lock()
+		params = self.call_buffer.get_nowait()
+		self.current_id -= 1
+		self.mutex.unlock()
+		print "call removed ", self.current_id
+		return params, cid
+
+	def result(self, cid):
+		""" return result of an call """
+		if self.is_finished(cid):
+			result = self.result_buffer[cid]
+			del self.result_buffer[cid]
+			return result
+		else:
+			raise Exception("Id not Found:"+str(cid))
+
+	def is_finished(self, cid):
+		""" if a call has finished """
+		return cid in self.result_buffer.keys()
+
+	def set_finished(self, cid, result=None):
+		""" set a call as finished"""
+		self.result_buffer[cid] = result
+
+	def empty(self):
+		""" """
+		return self.call_buffer.empty()
