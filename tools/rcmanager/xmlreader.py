9873fbb6807220f42a6873aca4bf254cb85968f8
Mayank Kumar <kr.mayank1997@gmail.com>
2017-08-14 01:10:27 +0530

    Update xmlreader.py

--- tools/rcmanager/xmlreader.py
+++ tools/rcmanager/xmlreader.py
@@ -9,10 +9,7 @@ def read_from_file(filename, printOnScreen=False):
         with open(filename) as f:
             data = f.read()
             if '.xml' in filename:
-                xml = xmltodict.parse(data)
-                if printOnScreen:
-                    print "XML file:", json.dumps(xml, indent=4, sort_keys=True)
-                return xml
+                return read_from_text(data, 'xml')
             elif '.json' in filename:
                 js = json.load(filename)
                 if printOnScreen:
@@ -34,3 +31,11 @@ def validate_xml(xml):
     except Exception, e:
         return False
     return True
+
+def get_text_from_file(filename):
+    try:
+        file = open(filename, 'r')
+        data = file.read()
+    except Exception, e:
+        raise e
+    return data
