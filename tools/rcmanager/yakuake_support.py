8cd4b8d53f965780040ce8bd8dc1f19669453619
Mayank Kumar <kr.mayank1997@gmail.com>
2017-08-22 22:57:54 +0530

    Update yakuake_support.py

--- tools/rcmanager/yakuake_support.py
+++ tools/rcmanager/yakuake_support.py
@@ -1,3 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  -----------------------
+#  -----  rcmanager  -----
+#  -----------------------
+#  An ICE component manager.
+#
+#    Copyright (C) 2009-2015 by RoboLab - University of Extremadura
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#
+
+#
+# CODE BEGINS
+#
 
 # Please refer to the following link for a nice documentation of the qdbus functions
 # http://urfoex.blogspot.com/2016/08/qdbus-create-tabs-in-yakuake-and-pidgin.html
