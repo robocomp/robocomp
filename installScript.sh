f480a607964d5daa73acabc11b1b90bbe3a52208
Maria Gabriela Bohórquez <gabriellabohorquez@gmail.com>
2018-07-09 22:25:15 -0400

    Added colors to terminal output

--- installScript.sh
+++ installScript.sh
@@ -2,6 +2,33 @@
 
 clear
 
+# Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+transparent="\e[0m"
+
+echo -e $blue"Welcome to the Installation Script for Robocomp!"                     
+
+echo -e $white"If you have any problems with the process, please contact us at robocomp.team@gmail.com"
+
+sleep 3
+
+# Check if --help flag is provided
+
+if [ ${#@} -ne 0 ] && [ "${@#"--help"}" = "" ]; then
+  printf -- 'For more information, please see the repository\n';
+  exit 0;
+fi;
+
 # ============================================================ #
 # =============     < Installing Robocomp >      ============= #
 # ============================================================ #
@@ -115,7 +142,8 @@ echo "Fetching meshes and textures. This could take a while."
 cd ~/robocomp
 git annex get .
 
-echo "The simulator is going to be launched to test. For information visit RoboComp Documentation"
+echo "The simulator is going to be launched to test it works."
+echo "For more information visit the repository."
 
 cd ~/robocomp/files/innermodel
 rcis simpleworld.xml
