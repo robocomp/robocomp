7697105981e1870ad620c79b492af93466d32cd4
Maria Gabriela Bohórquez <gabriellabohorquez@gmail.com>
2018-07-13 01:06:50 -0300

    Adding more steps and blank lines

--- installScript.sh
+++ installScript.sh
@@ -28,9 +28,11 @@ echo -e $white"
 
                                                                 " 
 
-echo -e $blue"Welcome to the Installation Script for Robocomp!\n"                     
+echo -e $blue"Welcome to the Installation Script for Robocomp!
+"                     
 
-echo -e $white"If you have any problems with the process, please contact us at robocomp.team@gmail.com\n"
+echo -e $white"If you have any problems with the process, please contact us at robocomp.team@gmail.com
+"
 
 sleep 3
 
@@ -45,14 +47,14 @@ fi;
 # =============     < Installing Robocomp >      ============= #
 # ============================================================ #
 
-echo "Updating system and Installing Dependencies"
+printf "Updating system and Installing Dependencies\nDepending on your internet connection, this might take a while."
 
 sudo apt-get update
 sudo apt-get install git git-annex cmake g++ libgsl0-dev libopenscenegraph-dev cmake-qt-gui zeroc-ice35 freeglut3-dev libboost-system-dev libboost-thread-dev qt4-dev-tools python-pip  python-pyparsing python-numpy python-pyside pyside-tools libxt-dev pyqt4-dev-tools libboost-test-dev libboost-filesystem-dev python-libxml2 python-xmltodict libccd-dev
 sudo pip install networkx
 sudo apt-get install yakuake qt4-designer
 
-echo "Installing Source from Github"
+printf "Installing Source from Github\n"
 
 git clone https://github.com/robocomp/robocomp.git
 cd ~/robocomp
@@ -76,16 +78,20 @@ while true; do
     " yn
     case $yn in
         [Yy]* ) option=1; break;;
-        [Nn]* ) option=0; echo "Not FCL_SUPPORT in Robocomp"; break;;
+        [Nn]* ) option=0; echo "No FCL_SUPPORT for Robocomp. The instalation process will keep going."; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
 if [ "$option" -eq "1" ]; then
+        
+        printf "Installing dependencies\n" 
         sudo aptitude install libeigen3-dev libboost-filesystem1.58-dev libboost-test-dev libboost-program-options-dev &&
 
+        printf "Creating new directory for the components.\n"
         mkdir software
         cd ~/software
+        printf "Downloading libccd.\n"
         git clone https://github.com/danfis/libccd.git
         cd libccd
         mkdir build
@@ -95,6 +101,7 @@ if [ "$option" -eq "1" ]; then
         sudo make install
 
         cd ~/software
+        printf "Downloading FCL.\n"
         git clone https://github.com/flexible-collision-library/fcl.git
         cd fcl
         mkdir build
@@ -104,6 +111,7 @@ if [ "$option" -eq "1" ]; then
         sudo make install
 
         cd ~/software
+        printf "Downloading libnano.\n" 
         git clone https://github.com/ethz-asl/libnabo.git
         cd libnabo
         mkdir build
@@ -113,6 +121,7 @@ if [ "$option" -eq "1" ]; then
         sudo make install
 
         cd ~/software
+        printf "Downloading libpointmatcher.\n"
         git clone https://github.com/ethz-asl/libpointmatcher.git
         cd libpointmatcher
         mkdir build
@@ -121,6 +130,7 @@ if [ "$option" -eq "1" ]; then
         make -j $numberCores
         sudo make install
         
+        printf "Installing FCL support.\n\n"
         cd ~/robocomp/build
         cmake-gui ..
         make -j $numberCores
@@ -166,11 +176,12 @@ if [ "$option" -eq "1" ]; then
     make
     cd bin
     sudo addgroup your-user dialout   # To solve some permissions issues in Ubuntu
+    printf "Installation complete. Launching component in the background.\n"
     ./startJoyStick.sh
 
 if [ "$option" -eq "0" ]; then    
     echo "We are installing the Keyboard component.
-    Tou will be able to move the robot around."
+    You will be able to move the robot around with the arrow buttons."
     cd ~/robocomp/components/robocomp-robolab/components/keyboardrobotcontroller
     cmake .
     make
@@ -189,8 +200,9 @@ git annex get .
 
 echo "The simulator is going to be launched to test if it works.
 "
-echo "For more information visit the repository."
+echo "For more information visit the repository and help."
 
+printf "Installation complete. Have a great time using Robocomp!\n"
 cd ~/robocomp/files/innermodel
 rcis simpleworld.xml
 
