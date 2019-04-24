f355489b92b609212ebeb38816fd46b69888143f
Esteban Martinena Guerrero <orensbruli@gmail.com>
2019-04-24 20:05:35 +0200

    Added docker command to test build.

--- tools/install/docker_building_test.sh
+++ tools/install/docker_building_test.sh
@@ -1,7 +1,10 @@
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' docker-ce|grep "install ok installed")
-echo "Checking for package: $PKG_OK"
+echo "Checking for package docker-ce: $PKG_OK"
 if [ -z "$PKG_OK" ]
 then
-  echo "No somelib. Setting up somelib."
+  echo "No docker-ce. Setting up somelib."
   sudo apt-get --force-yes --yes install the.package.name
 fi
+sudo docker run -ti -w /home/robolab/ --user robolab:robolab --rm -v $(pwd)/robocomp_install.sh:/home/robolab/robocomp_install.sh robocomp/clean-testing:robocomp-ubuntu18.04  bash -x robocomp_install.sh; echo $?
+
+
