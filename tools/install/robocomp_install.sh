a1cd7c4cd8949dacef4d3f49130df695b0fafe23
Esteban Martinena Guerrero <orensbruli@gmail.com>
2019-04-30 18:25:41 +0200

    Fixed bug with the robocomp version on git command

--- tools/install/robocomp_install.sh
+++ tools/install/robocomp_install.sh
@@ -1,9 +1,18 @@
+
+if [ -z "$1" ] || [ "$1" != 'stable' ] && [ "$1" != 'development' ]
+then
+    echo "Currently you only can use stable or development as robocomp branches."
+    ROBOCOMP_BRANCH='stable'
+else
+    ROBOCOMP_BRANCH=$1
+fi
+
 sudo apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git git-annex cmake g++ libgsl-dev libopenscenegraph-dev cmake-qt-gui zeroc-ice-all-dev freeglut3-dev libboost-system-dev libboost-thread-dev qt4-dev-tools python-pip  python-pyparsing python-numpy python-pyside pyside-tools libxt-dev libboost-test-dev libboost-filesystem-dev python-libxml2 python-xmltodict libccd-dev python-zeroc-ice zeroc-ice-all-runtime
 sudo pip install networkx
 alias python=python2
-git clone https://github.com/robocomp/robocomp.git
+git clone -b $ROBOCOMP_BRANCH https://github.com/robocomp/robocomp.git
 sudo ln -s ~ /home/robocomp
 echo "export ROBOCOMP=~/robocomp" >> ~/.bashrc
 echo "export PATH=$PATH:/opt/robocomp/bin" >> ~/.bashrc
