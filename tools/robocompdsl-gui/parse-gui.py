80a1094b333d9ad738a236cc0ceea423b1088eb9
elebarr <50713548+elebarr@users.noreply.github.com>
2019-06-24 10:42:50 +0200

    Update parse-gui.py

--- tools/robocompdsl-gui/parse-gui.py
+++ tools/robocompdsl-gui/parse-gui.py
@@ -36,32 +36,38 @@ dictionary = load_all_interfaces("/opt/robocomp/interfaces/IDSLs")
 #print(dictionary["DifferentialRobot"])
 
 
-def check_imported_interfaces(cdslFile):
-    if cdslFile.endswith(".cdsl"):
-        # check if the imported interfaces are valid
-        inputText = open(cdslFile, 'r').read()
-        file_dict = CDSLParsing.fromString(inputText) #solve, it already checks if interface exists
-        for f in file_dict['imports']:
-            f = f[:-5]
-            if f not in dictionary: #unreachable code, already checked
-                print("Interface not found in database")
-                return
+def check_imported_interfaces(cdsl_dictionary):
+    # check if the imported interfaces are valid
+    for f in cdsl_dictionary['imports']:
+        f = f[:-5]
+        if f not in dictionary: #unreachable code, already checked
+            print("Interface not found in database")
+            return
 
-        #check if the communication interfaces were imported
-        imported_interfaces_list = file_dict['imports']
-        communication_interfaces_list =  file_dict['requires']+file_dict['implements']+file_dict['subscribesTo']+file_dict['publishes']
+    #check if the communication interfaces were imported
+    imported_interfaces_list = cdsl_dictionary['imports']
+    communication_interfaces_list =  cdsl_dictionary['requires']+cdsl_dictionary['implements']+cdsl_dictionary['subscribesTo']+cdsl_dictionary['publishes']
 
-        for interface_required in communication_interfaces_list:
-            interface_required = interface_required+".idsl"
-            if interface_required not in (imported_interfaces_list):
-                return interface_required
-    else:
-        print ("Invalid file extension")
+    for interface_required in communication_interfaces_list:
+        interface_required = interface_required+".idsl"
+        if interface_required not in (imported_interfaces_list):
+            return interface_required
+
     return None
 
 def check_file(cdslFile):
-    interface_missing = check_imported_interfaces(cdslFile)
-    if interface_missing != None:
-        print ("Interface " + interface_missing + " must be imported")
+    if cdslFile.endswith(".cdsl"):
+        inputText = open(cdslFile, 'r').read()
+        file_dict = CDSLParsing.fromString(inputText)  # solve, it already checks if interface exists
+
+        interface_missing = check_imported_interfaces(file_dict)
+        if interface_missing != None:
+            print ("Interface " + interface_missing + " must be imported")
+
+        if file_dict['innermodelviewer'] == True and file_dict['language'] != 'cpp' and file_dict['gui'][0] != 'Qt' :
+            print("Incompatible innermodelViewer configuration")
+
+    else:
+        print("Invalid file extension")
 
 check_file("eleComp.cdsl")
