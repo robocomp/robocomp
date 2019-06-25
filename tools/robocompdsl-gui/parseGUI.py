81ac06c75ca0e90be7c50b7cd6e35b1b70f51a64
elebarr <50713548+elebarr@users.noreply.github.com>
2019-06-25 11:24:21 +0200

    Update parseGUI.py

--- tools/robocompdsl-gui/parseGUI.py
+++ tools/robocompdsl-gui/parseGUI.py
@@ -3,31 +3,9 @@
 import os
 from parseIDSL import IDSLParsing
 from parseCDSL import CDSLParsing
+from parseCDSL import LoadInterfaces
 
-class loadInterfaces:
-
-    def get_interfaces_name(file):
-        names = []
-        idsl_content = IDSLParsing.fromFileIDSL(file)
-        for content in idsl_content['module']['contents']:
-            if content[0] == "interface":
-                names.append(content[1])
-        return names
-
-    def load_all_interfaces(self, path):
-        interfaces = {}
-        for r, d, f in os.walk(path):
-            for file in f:
-                if '.idsl' in file:
-                    names = self.get_interfaces_name(os.path.join(r, file))
-                    for name in names:
-                        if name in interfaces:
-                            interfaces[name].append(file)
-                        else:
-                            interfaces[name] = [file]
-        return interfaces
-
-dictionary = loadInterfaces.load_all_interfaces(loadInterfaces, "/opt/robocomp/interfaces/IDSLs")
+dictionary = LoadInterfaces.load_all_interfaces(LoadInterfaces, "/opt/robocomp/interfaces/IDSLs")
 #print( "Todas las posibilidades\n", dictionary)
 
 #obtener el fichero para un interfaz específico
@@ -41,11 +19,11 @@ def check_imported_interfaces(cdsl_dictionary):
     imported_interfaces_list = cdsl_dictionary['imports']
     communication_interfaces_list = cdsl_dictionary['requires'] + cdsl_dictionary['implements'] + cdsl_dictionary[
         'subscribesTo'] + cdsl_dictionary['publishes']
-    interfacesToImp = [] #interfaces to be imported
+    interfacesToImp = [] #required interfaces to be imported
 
     #Search which interfaces must be imported
     for r in communication_interfaces_list:#for each interface in communication
-        for d in dictionary[r]:#for each file to be import from each interface
+        for d in dictionary[r]:#for each file to be imported from each interface
             interfacesToImp.append(d)
 
     for interface_required in interfacesToImp:
@@ -71,7 +49,3 @@ def check_file(cdslFile):
         print("Invalid file extension")
 
 check_file("eleComp.cdsl")
-
-#Encapsular los dos metodos en una clase en el parser y borrarlos de aqui
-#Comprobar por que cuando introduzco una interfaz inventada no informa correctamente del error
-#Recordar que tengo que cambiar la ruta de acceso a los idsl
