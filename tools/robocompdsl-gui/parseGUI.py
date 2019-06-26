42e2fe511e0a7d80734a778709a9d3a0de29b597
elebarr <50713548+elebarr@users.noreply.github.com>
2019-06-26 17:00:34 +0200

    Update parseGUI.py

--- tools/robocompdsl-gui/parseGUI.py
+++ tools/robocompdsl-gui/parseGUI.py
@@ -33,10 +33,25 @@ def check_imported_interfaces(cdsl_dictionary):
 
     return None
 
+def get_interfaces_from_file(inputFile):
+    interfaces = []
+    for interf, files_list in dictionary.items():
+        for file in files_list:
+            if file == inputFile:
+                interfaces.append(interf)
+    return interfaces
+
+def get_files_from_interface(inputInterface):
+    files = []
+    for i in dictionary[inputInterface]:
+        files.append(i)
+    return files
+
+
 def check_file(cdslFile):
     #comprobar fichero y como resultado devolver lista con < nº_linea, texto, error > mostrar por pantalla
-
     if cdslFile.endswith(".cdsl"):
+
         file_errors = []
         inputText = open(cdslFile, 'r').read()
         file_dict = CDSLParsing.fromString(inputText)
@@ -51,10 +66,14 @@ def check_file(cdslFile):
     else:
         print("Invalid file extension")
 
-check_file("cdslFiles/eleComp.cdsl")
+
+get_interfaces_from_file("CGR.idsl")
+get_files_from_interface("MSKBodyEvent")
+#check_file("cdslFiles/eleComp.cdsl")
 #check_file("cdslFiles/Comp1.cdsl")
 #check_file("cdslFiles/Comp2.cdsl")
 #check_file("cdslFiles/Comp3.cdsl")
 #check_file("cdslFiles/Comp4.cdsl")
 #check_file("cdslFiles/Comp5.cdsl")
 #check_file("cdslFiles/Comp6.cdsl")
+#check_file("cdslFiles/Comp7.cdsl")
