6ce82dcaad2176adba0afc1c77f69adacdd15d7a
elebarr <elebrrg@gmail.com>
2019-07-08 12:11:55 +0200

    main2.py restructured

--- tools/robocompdsl-gui/main2.py
+++ tools/robocompdsl-gui/main2.py
@@ -2,11 +2,81 @@
 
 import sys
 
-#from PySide2.QtWidgets import QApplication, QMainWindow, QTextEdit, QFileDialog, QWidget
-#from PySide2.QtCore import QFile, QRegExp, Qt
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QMenu, QMessageBox, QTextEdit, QWidget, QGridLayout
+from PySide2.QtWidgets import QApplication, QMainWindow, QTextEdit, QFileDialog, QWidget
+from PySide2.QtCore import QFile, QRegExp, Qt, QRegExp
+from PySide2.QtGui import QSyntaxHighlighter, QFont, QTextCharFormat
 from ui_gui import Ui_MainWindow
+
+class Highlighter(QSyntaxHighlighter):
+    def __init__(self, parent=None):
+        super(Highlighter, self).__init__(parent)
+
+        keywordFormat = QTextCharFormat()
+        keywordFormat.setForeground(Qt.darkBlue)
+        keywordFormat.setFontWeight(QFont.Bold)
+
+        keywordPatterns = ["\\bimport\\b", "\\bComponent\\b", "\\bCommunications\\b",
+                "\\bpublishes\\b", "\\bimplements\\b", "\\bsubscribesTo\\b", "\\brequires\\b",
+                "\\blanguage\\b", "\\bgui\\b", "\\boptions\\b","\\bInnerModelViewer\\b","\\bstatemachine\\b","\\bmodules\\b"]
+
+        self.highlightingRules = [(QRegExp(pattern), keywordFormat)
+                for pattern in keywordPatterns]
+
+        classFormat = QTextCharFormat()
+        classFormat.setFontWeight(QFont.Bold)
+        classFormat.setForeground(Qt.darkMagenta)
+        self.highlightingRules.append((QRegExp("\\bQ[A-Za-z]+\\b"),
+                classFormat))
+
+        singleLineCommentFormat = QTextCharFormat()
+        singleLineCommentFormat.setForeground(Qt.red)
+        self.highlightingRules.append((QRegExp("//[^\n]*"),
+                singleLineCommentFormat))
+
+        self.multiLineCommentFormat = QTextCharFormat()
+        self.multiLineCommentFormat.setForeground(Qt.red)
+
+        quotationFormat = QTextCharFormat()
+        quotationFormat.setForeground(Qt.darkGreen)
+        self.highlightingRules.append((QRegExp("\".*\""), quotationFormat))
+
+        functionFormat = QTextCharFormat()
+        functionFormat.setFontItalic(True)
+        functionFormat.setForeground(Qt.blue)
+        self.highlightingRules.append((QRegExp("\\b[A-Za-z0-9_]+(?=\\()"),
+                functionFormat))
+
+        self.commentStartExpression = QRegExp("/\\*")
+        self.commentEndExpression = QRegExp("\\*/")
+
+    def highlightBlock(self, text):
+        for pattern, format in self.highlightingRules:
+            expression = QRegExp(pattern)
+            index = expression.indexIn(text)
+            while index >= 0:
+                length = expression.matchedLength()
+                self.setFormat(index, length, format)
+                index = expression.indexIn(text, index + length)
+
+        self.setCurrentBlockState(0)
+
+        startIndex = 0
+        if self.previousBlockState() != 1:
+            startIndex = self.commentStartExpression.indexIn(text)
+
+        while startIndex >= 0:
+            endIndex = self.commentEndExpression.indexIn(text, startIndex)
+
+            if endIndex == -1:
+                self.setCurrentBlockState(1)
+                commentLength = len(text) - startIndex
+            else:
+                commentLength = endIndex - startIndex + self.commentEndExpression.matchedLength()
+
+            self.setFormat(startIndex, commentLength,
+                    self.multiLineCommentFormat)
+            startIndex = self.commentStartExpression.indexIn(text,
+                    startIndex + commentLength);
 
 
 class RoboCompDSLGui(QMainWindow):
@@ -15,6 +85,16 @@ class RoboCompDSLGui(QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.setupEditor()
+
+    def setupEditor(self):
+        font = QFont()
+        font.setFamily('Courier')
+        font.setFixedPitch(True)
+        font.setPointSize(10)
+
+        self.highlighter = Highlighter(self.ui.mainTextEdit.document())
 
 
 if __name__ == "__main__":
