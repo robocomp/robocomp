614fa622da21c049c36cde7075e6e1b77a3269ce
elebarr <elebrrg@gmail.com>
2019-07-19 09:56:18 +0200

    check_file adapted to main2.py

--- tools/robocompdsl-gui/parseGUI.py
+++ tools/robocompdsl-gui/parseGUI.py
@@ -49,51 +49,67 @@ dictionary = LoadInterfaces.load_all_interfaces(LoadInterfaces, "/opt/robocomp/i
 #print("Fichero para DifferentialRobot")
 #print(dictionary["DifferentialRobot"])
 
-def check_imported_interfaces(cdsl_dictionary):
-    imported_interfaces_list = cdsl_dictionary['imports']
-    communication_interfaces_list = cdsl_dictionary['requires'] + cdsl_dictionary['implements'] + cdsl_dictionary[
-        'subscribesTo'] + cdsl_dictionary['publishes']
-    files_to_import = [] #required idsl files to be imported
+class FileChecker:
+    def check_imported_interfaces(cdsl_dictionary):
+        imported_interfaces_list = cdsl_dictionary['imports']
+        communication_interfaces_list = cdsl_dictionary['requires'] + cdsl_dictionary['implements'] + cdsl_dictionary[
+            'subscribesTo'] + cdsl_dictionary['publishes']
+        files_to_import = [] #required idsl files to be imported
 
-    for com in communication_interfaces_list:
-        files = LoadInterfaces.get_files_from_interface(com)
-        files_to_import.extend(files)
+        for com in communication_interfaces_list:
+            files = LoadInterfaces.get_files_from_interface(com)
+            files_to_import.extend(files)
 
-    for file_required in files_to_import:
-        if file_required not in (imported_interfaces_list):
-            return file_required
-    return None
+        for file_required in files_to_import:
+            if file_required not in (imported_interfaces_list):
+                return file_required
+        return None
 
-def check_file(cdslFile):
-    #comprobar fichero y como resultado devolver lista con < nº_linea, texto, error > mostrar por pantalla
-    if cdslFile.endswith(".cdsl"):
+    def check_file(self, cdslFile):
+        #comprobar fichero y como resultado devolver lista con < nº_linea, texto, error > mostrar por pantalla
+        if cdslFile.endswith(".cdsl"):
 
-        file_errors = []
-        inputText = open(cdslFile, 'r').read()
+            file_errors = []
+            inputText = open(cdslFile, 'r').read()
+            file_dict = CDSLParsing.fromString(inputText)
+
+            interface_missing = self.check_imported_interfaces(file_dict)
+            if interface_missing != None:#comprobar si es lista
+                if isinstance(interface_missing, list):
+                    for i in interface_missing:
+                        print ("Interface " + i + " must be imported")
+                else:
+                    print ("Interface "+ interface_missing + " must be imported")
+
+            if file_dict['innermodelviewer'] == True and file_dict['language'] != 'cpp' and file_dict['gui'][0] != 'Qt' :
+                print("Incompatible innermodelViewer configuration")
+
+        else:
+            print("Invalid file extension")
+
+    #To test main2
+    def check_text(self, inputText):
         file_dict = CDSLParsing.fromString(inputText)
 
-        interface_missing = check_imported_interfaces(file_dict)
-        if interface_missing != None:#comprobar si es lista
+        interface_missing = self.check_imported_interfaces(file_dict)
+        if interface_missing != None:  # comprobar si es lista
             if isinstance(interface_missing, list):
                 for i in interface_missing:
-                    print ("Interface " + i + " must be imported")
+                    print("Interface " + i + " must be imported")
             else:
-                print ("Interface "+ interface_missing + " must be imported")
+                print("Interface " + interface_missing + " must be imported")
 
-        if file_dict['innermodelviewer'] == True and file_dict['language'] != 'cpp' and file_dict['gui'][0] != 'Qt' :
+        if file_dict['innermodelviewer'] == True and file_dict['language'] != 'cpp' and file_dict['gui'][0] != 'Qt':
             print("Incompatible innermodelViewer configuration")
-
-    else:
-        print("Invalid file extension")
 
 
 #get_interfaces_from_file("CGR.idsl")
 #get_files_from_interface("MSKBodyEvent")
-#check_file("cdslFiles/eleComp.cdsl")
-#check_file("cdslFiles/Comp1.cdsl")
-#check_file("cdslFiles/Comp2.cdsl")
-#check_file("cdslFiles/Comp3.cdsl")
-#check_file("cdslFiles/Comp4.cdsl")
-#check_file("cdslFiles/Comp5.cdsl")
-#check_file("cdslFiles/Comp6.cdsl")
-#check_file("cdslFiles/Comp7.cdsl")
+#FileChecker.check_file(fileChecker, "cdslFiles/eleComp.cdsl")
+#FileChecker.check_file(fileChecker,"cdslFiles/Comp1.cdsl")
+#FileChecker.check_file(fileChecker,"cdslFiles/Comp2.cdsl")
+#FileChecker.check_file(fileChecker,"cdslFiles/Comp3.cdsl")
+#FileChecker.check_file(fileChecker,"cdslFiles/Comp4.cdsl")
+#FileChecker.check_file(fileChecker,"cdslFiles/Comp5.cdsl")
+#FileChecker.check_file(fileChecker,"cdslFiles/Comp6.cdsl")
+#FileChecker.check_file(fileChecker,"cdslFiles/Comp7.cdsl")
