c7e1f9a41553a818c4e27eb25d91509a5a3412b3
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-03-29 17:02:05 +0200

    [robocompdsl] Added idsl_creation test

--- tools/robocompdsl/test/test_robocompdsl.py
+++ tools/robocompdsl/test/test_robocompdsl.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import unittest
+import robocompdsl
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 ROBOCOMPDSL_DIR = os.path.join(CURRENT_DIR, "..")
+RESOURCES_DIR = os.path.join(CURRENT_DIR, "resources")
 TEST_DIR = os.path.join(CURRENT_DIR, "..", "autogeneration_tests", "test_cdsl")
 sys.path.append(ROBOCOMPDSL_DIR)
 
@@ -11,9 +13,27 @@ from autogeneration_tests.test_cdsl.test_component_generation import ComponentGe
 
 
 class ParsingUtilsTest(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
     def test_component_generation_checker(self):
         checker = ComponentGenerationChecker()
         self.assertTrue(checker.check_components_generation(TEST_DIR, False, False, ignore="test_agmTestCpp"))
+
+    def test_idsl_creation(self):
+        input_file = os.path.join(RESOURCES_DIR, "InnerModelManager.idsl")
+        output_file = os.path.join(RESOURCES_DIR, "InnerModelManager_generated.ice")
+        truth_file = os.path.join(RESOURCES_DIR, "InnerModelManager.ice")
+        robocompdsl.generate_idsl_file(input_file, output_file, [])
+        # self.assertTrue(filecmp.cmp(output_file, truth_file))
+        self.assertFilesSame(output_file, truth_file)
+        # os.remove(output_file)
+
+    def assertFilesSame(self, path1, path2):
+        with open(path1, 'r') as f1, open(path2, 'r') as f2:
+            text1 = f1.readlines()
+            text2 = f2.readlines()
+        self.assertEqual(text1, text2)
 
 
 # component = specific_parsers.DSLFactory().from_file("/home/robolab/robocomp/components/robocomp-robolab/components/hardware/camera/camerasimple/camerasimple.cdsl")
