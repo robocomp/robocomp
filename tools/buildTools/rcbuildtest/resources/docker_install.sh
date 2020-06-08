e315aae4b9e6fc84ed85110ef3cb944e97abe892
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-06-08 12:07:04 +0200

    [tools] added echo commands to the dockers_intall.sh file.

--- tools/buildTools/rcbuildtest/resources/docker_install.sh
+++ tools/buildTools/rcbuildtest/resources/docker_install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+echo "sudo apt-get update"
 sudo apt-get update
+echo "sudo apt-get --yes install apt-transport-https ca-certificates curl gnupg-agent software-properties-common"
 sudo apt-get --yes install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
+echo "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\""
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo "sudo apt-get update"
 sudo apt-get update
+echo "sudo apt-get --yes install docker-ce docker-ce-cli containerd.io"
 sudo apt-get --yes install docker-ce docker-ce-cli containerd.io
+echo "sudo groupadd docker"
 sudo groupadd docker
+echo "sudo gpasswd -a $USER docker"
 sudo gpasswd -a $USER docker
+echo "newgrp docker"
 newgrp docker
