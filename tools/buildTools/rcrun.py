b51a43da2a8dc485c5d79e92bb0a2597fe761c30
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-07-14 11:28:41 +0200

    [tools/buildTools] some build tools updated to use yakuake.

--- tools/buildTools/rcrun.py
+++ tools/buildTools/rcrun.py
@@ -4,59 +4,74 @@ from __future__ import print_function
 import argparse, argcomplete
 import os
 import string
-from workspace import workspace as WS
+import sys
+sys.path.append('/opt/robocomp/python')
+from workspace import Workspace
+from yaku_lib import Yaku
 
-def complete_components(prefix, parsed_args, **kwargs):
-    components = WS.list_components_in_workspace(WS.workspace_paths)
-    componentsname=[]
-    for component in components:
-        componentsname.append(component.split('/')[ len(component.split('/')) -1 ])
-    return (componentname for componentname in componentsname if componentname.startswith(prefix))
 
-def complete_scripts(prefix, parsed_args, **kwargs):
-    configPath = WS.find_component_exec('testcomp1')
-    if not configPath: return
-    configPath = configPath[:-4]+ '/etc'
-    configFiles = []
-    for root, dirs, files in os.walk(configPath): configFiles = files
-    return (configfile for configfile in configFiles if configfile.startswith(prefix))
+class rcrun:
+    def __init__(self, workspace=None):
+        if not workspace:
+            workspace = Workspace()
+        self.ws = workspace
 
-def find_script(action,component):
-    paths = WS.find_component_exec(component)
-    pathsrc = WS.find_component_src(component)
-    pathsrc = [ os.path.join(x,'bin') for x in pathsrc]
-    pathsrc.append(paths)
-    for path in pathsrc:
-        for file in os.listdir(path):
-            if file[-3:] == '.sh' or file[-5:] == '.bash':
-                if string.lower(file[:len(action)])==string.lower(action):
-                    return os.path.join(path,file)
-    return False
+    def complete_components(self, prefix):
+        components = self.ws.list_filtered_components_names(prefix)
+        componentsname=[]
+        for component in components:
+            componentsname.append(component.split('/')[ len(component.split('/')) -1 ])
+        return (componentname for componentname in componentsname if componentname.startswith(prefix))
+
+    def complete_scripts(self, prefix, parsed_args, **kwargs):
+        configPath = self.ws.find_component_exec('testcomp1')
+        if not configPath: return
+        configPath = configPath[:-4]+ '/etc'
+        configFiles = []
+        for root, dirs, files in os.walk(configPath): configFiles = files
+        return (configfile for configfile in configFiles if configfile.startswith(prefix))
+
+    def find_script(self, action,component):
+        paths = self.ws.find_component_exec(component)
+        pathsrc = self.ws.find_component_src(component)
+        pathsrc = [ os.path.join(x,'bin') for x in pathsrc]
+        pathsrc.append(paths)
+        for path in pathsrc:
+            for file in os.listdir(path):
+                if file[-3:] == '.sh' or file[-5:] == '.bash':
+                    if string.lower(file[:len(action)])==string.lower(action):
+                        return os.path.join(path,file)
+        return False
 
 def main():
     parser = argparse.ArgumentParser(description="Locate and run a robocomp component")
-    group = parser.add_mutually_exclusive_group(required = True)
+    group = parser.add_mutually_exclusive_group(required=False)
     cgroup = parser.add_mutually_exclusive_group()
-    parser.add_argument('component', help='component name').completer = complete_components
+    ws = Workspace()
+    rcrun_instance = rcrun(ws)
+    parser.add_argument('component', help='component name').completer = rcrun_instance.complete_components
     group.add_argument('-s', '--start', action = 'store_true' , help="start component")
     group.add_argument('-st', '--stop', action = 'store_true' , help="stop component")
     group.add_argument('-fst', '--fstop', action = 'store_true' , help=" force stop component")
     cgroup.add_argument('-d', '--debug', action = 'store_true' , help="start a component in debug mode")
     cgroup.add_argument('-cf', '--cfile', nargs = 1 , help="use custom ice config file (absolute path)")
-    cgroup.add_argument('-c', '--config', nargs = 1 , help="ice config file to choose from default config directory").completer = complete_scripts
+    cgroup.add_argument('-c', '--config', nargs = 1 , help="ice config file to choose from default config directory").completer = rcrun_instance.complete_scripts
     parser.add_argument('-is','--ignore_scripts', action='store_true',help="ignore all the script files if found")
     
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
 
     component = args.component
-    
-    #if stop command no need for searching and all
+
+    if not args.start and not args.stop and not args.fstop:
+        args.start = True
+
+    #if stop command no need for searching all
     if args.stop:
-        if not WS.find_component_exec(component):
+        if not ws.find_component_exec(component):
             args.error("couldnt find the component %s in any of the workspaces" % (args.stop[0]))
             return
-        stpath = find_script("stop",component);
+        stpath = rcrun_instance.find_script("stop", component)
         if stpath and not args.ignore_scripts:
             #print("using script {0}".format(stpath))
             command = stpath
@@ -65,10 +80,10 @@ def main():
         os.system(command)
         return
     elif args.fstop:
-        if not WS.find_component_exec(component):
+        if not ws.find_component_exec(component):
             parser.error("couldnt find the component %s in any of the workspaces" % (args.fstop[0]))
             return
-        sfpath = find_script("forcestop",component);
+        sfpath = rcrun_instance.find_script("forcestop",component)
         if sfpath and not args.ignore_scripts:
             #print("using script {0}".format(sfpath))
             command = sfpath
@@ -79,16 +94,16 @@ def main():
     
     
     #search for the component
-    componentPath = WS.find_component_exec(component)
-    
-    if not componentPath:
-        if not WS.find_component_src(component):
-            print("couldnt find the component %s in any of the workspaces" % (component))
-        else:
-            print("couldnt find the target please build it first using rcbuild %s " % (component))
+    component_path = ws.find_component(component)
+    component = component_path.rstrip(os.path.sep).split(os.path.sep)[-1]
+    if not component_path:
+        print("Couldn't find the component path for %s in any of the workspaces" % (component))
         return
+    component_exec_path = ws.find_component_exec_file(component_path)
+    component_etc_path = ws.find_component_etc_path(component_path)
 
-    componentPathetc = componentPath[:-4]
+    
+
 
     #find the config file
     '''
@@ -98,57 +113,54 @@ def main():
         else we will use a random file
         user defined config file will override all the above
     '''
-    configFiles = []
-    configPath = componentPathetc + '/etc'
+    config_files = []
     ice_config = ""
-    for root, dirs, files in os.walk(configPath): configFiles = files
-    if len(configFiles)==0:
-        parser.error("couldnt find any config file in the etc directory, please specify a custom config file\n")
-    else:
-        if args.debug:
-            for file in configFiles:
-                if file.endswith('.debug'): ice_config = configPath + "/" + file    
-            if len(ice_config) < 9:
-                parser.error("couldnt find any debug config file in the etc directory, please specify a custom config file\n")  
+    for root, dirs, config_files in os.walk(component_etc_path):
+        if len(config_files) == 0:
+            parser.error("couldnt find any config file in the etc directory, please specify a custom config file\n")
         else:
-            if len(configFiles)==1 :
-                ice_config = configPath + "/" + configFiles[0]
+            if args.debug:
+                for file in config_files:
+                    if file.endswith('.debug'):
+                        ice_config = os.path.join(component_etc_path, file)
+                if len(ice_config) < 9:
+                    parser.error("Couldn't find any debug config file in the etc directory, please specify a custom config file\n")
             else:
-                if "config" in configFiles:
-                    ice_config = configPath + "/config"
-                elif 'generic_config' in  configFiles:
-                    ice_config = configPath + "/generic_config"
+                if len(config_files) == 1:
+                    ice_config = os.path.join(component_etc_path, config_files[0])
+                elif len(config_files) > 1:
+                    if "config" in config_files:
+                        ice_config = os.path.join(component_etc_path, "config")
+                    elif 'generic_config' in  config_files:
+                        ice_config = os.path.join(component_etc_path, "config")
+                    else:
+                        ice_config = os.path.join(component_etc_path, config_files[0])
                 else:
-                    ice_config = configPath + "/" + configFiles[0]
+                    parser.error(
+                        "Couldn't find any config file in the etc directory, please specify a custom config file\n")
     
     if args.config:
-        if args.config[0] in configFiles:
-            ice_config = configPath + "/" + args.config[0]
+        if args.config[0] in config_files:
+            ice_config = os.path.join(component_etc_path, args.config[0])
         else:
             parser.error("couldnt find config file '{0}' in the etc directory".format(args.config[0]))
     elif args.cfile:
         ice_config = args.config[0]
 
     #execute the command
-    spath = find_script("start",component);
-    sdpath = find_script("startdebug",component);
+    # spath = rcrun_instance.find_script("start",component);
+    # sdpath = rcrun_instance.find_script("startdebug",component);
     
     if args.start:
-        if spath and not (args.config or args.cfile or args.debug) and not args.ignore_scripts:
-            command = spath
-        elif sdpath and args.debug  and not args.ignore_scripts:
-            command = sdpath
-        else:
-            command = componentPath + "/" + string.lower(component) + " --Ice.Config=" + ice_config
-    else:
-        if spath and not (args.config or args.cfile or args.debug) and not args.ignore_scripts:
-            command = spath
-        elif sdpath and args.debug and not args.ignore_scripts:
-            command = sdpath
-        else:
-            command = componentPath + "/" + string.lower(component) + " --Ice.Config=" + ice_config
+        command = "cd "+component_path+" && "
+        command += component_exec_path + " " + ice_config
+        if component_exec_path.endswith('.py'):
+            command = "python "+command
+
+
     
     print("executing : "+command)
+    Yaku.rename_current_tab(name=component)
     os.system(command)
 
 if __name__ == '__main__':
