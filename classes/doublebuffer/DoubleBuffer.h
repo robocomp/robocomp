449fd5c752d65b70628706d1833519b9141293c0
Juan Carlos García García <juancarlos97gg@gmail.com>
2020-07-22 13:41:45 +0200

    Remove DefaultConverter from DoubleBuffer

--- classes/doublebuffer/DoubleBuffer.h
+++ classes/doublebuffer/DoubleBuffer.h
@@ -11,114 +11,116 @@
 #include <atomic>
 
 //Función utilizada como argumento por defecto.
-constexpr auto empty_fn =  [](auto& I, auto& T) {};
+constexpr auto empty_fn = [](auto &I, auto &T) {};
 
-template <class I, class O> 
+template <typename T, typename = void>
+struct is_iterable : std::false_type
+{
+};
+template <typename T>
+//especialización del template si tiene función begin y end.
+struct is_iterable<T, std::void_t<decltype(std::declval<T>().begin()), decltype(std::declval<T>().end())>> : std::true_type
+{
+};
+
+template <class I, class O>
 class Converter
 {
     public:
-        virtual bool ItoO(const I & iTypeData, O &oTypeData, std::function<void(const I&, O&)> t = empty_fn)=0;
-        virtual bool OtoI(const O & oTypeData, I &iTypeData, std::function<void(I&, const O&)> t = empty_fn)=0;
-		//virtual bool clear(O & oTypeData)=0;
-};
-
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
-template <typename T>
-//especialización del template si tiene función begin y end.
-struct is_iterable<T, std::void_t<decltype(std::declval<T>().begin()), decltype(std::declval<T>().end())>> : std::true_type {};
- 
-template <class I, class O>
-class ConverterDefault : Converter<I,O>
-{   
-    public:
-
-        bool ItoO(const I & iTypeData, O &oTypeData, std::function<void(const I&, O&)> t = empty_fn) 
-        { 
+        bool ItoO(const I &iTypeData, O &oTypeData, std::function<void(const I &, O &)> t = empty_fn)
+        {
             //Si es el mismo tipo o es convertible de I a O
-            if constexpr (std::is_same<I, O>::value || std::is_convertible<I, O>::value) 
+            if constexpr (std::is_same<I, O>::value || std::is_convertible<I, O>::value)
             {
                 oTypeData = iTypeData;
-            } 
-            else if constexpr (is_iterable<I>::value && is_iterable<O>::value) 
+            }
+            else if constexpr (is_iterable<I>::value && is_iterable<O>::value)
             { //Si es iterable, hay que ver si esto es suficiente
 
-                    //Conseguimos el tipo que contiene el iterador
-                    using I_T = typename std::decay<decltype(*iTypeData.begin())>::type;
-                    using O_T = typename std::decay<decltype(*oTypeData.begin())>::type;
+                //Conseguimos el tipo que contiene el iterador
+                using I_T = typename std::decay<decltype(*iTypeData.begin())>::type;
+                using O_T = typename std::decay<decltype(*oTypeData.begin())>::type;
 
-                    //Comprobamos si se puede convertir el I_T a O_T 
-                    if constexpr(std::is_convertible<I_T, O_T>::value) 
-                    {
-                        // No funciona con copy; 
-                        oTypeData = std::vector<O_T> (iTypeData.begin(), iTypeData.end());
-                    } 
-                    else 
-                    {
-                        //Si no es convertible necesitamos una función para convertir de I a O.
-                        //Comprobamos que el tipo de la función t es distinto del tipo de empty_fn (Las lambdas tienen tipos únicos).
-                        static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value , "A function needs to be implemented to transform OtoI and ItoO");
-                        //Llamamos a la función
-                        t(iTypeData, oTypeData);
-                    }
-            } else 
+                //Comprobamos si se puede convertir el I_T a O_T
+                if constexpr (std::is_convertible<I_T, O_T>::value)
+                {
+                    // No funciona con copy;
+                    oTypeData = std::vector<O_T>(iTypeData.begin(), iTypeData.end());
+                }
+                else
+                {
+                    //Si no es convertible necesitamos una función para convertir de I a O.
+                    //Comprobamos que el tipo de la función t es distinto del tipo de empty_fn (Las lambdas tienen tipos únicos).
+                    static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value, "A function needs to be implemented to transform OtoI and ItoO");
+                    //Llamamos a la función
+                    t(iTypeData, oTypeData);
+                }
+            }
+            else
             {
                 //Lo mismo que antes.
-                static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value , "A function needs to be implemented to transform OtoI and ItoO");
+                static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value, "A function needs to be implemented to transform OtoI and ItoO");
                 t(iTypeData, oTypeData);
             }
             return true;
         };
-        
-        bool OtoI(const O & oTypeData, I &iTypeData, std::function<void(I&,const O&)> t = empty_fn ) 
-        { 
 
-            if constexpr (std::is_same<I, O>::value  || std::is_convertible<I, O>::value)
-                iTypeData = oTypeData; 
-            else  if constexpr (is_iterable<I>::value && is_iterable<O>::value) {
+        bool OtoI(const O &oTypeData, I &iTypeData, std::function<void(I &, const O &)> t = empty_fn)
+        {
+
+            if constexpr (std::is_same<I, O>::value || std::is_convertible<I, O>::value)
+                iTypeData = oTypeData;
+            else if constexpr (is_iterable<I>::value && is_iterable<O>::value)
+            {
 
                 using I_T = typename std::decay<decltype(*iTypeData.begin())>::type;
                 using O_T = typename std::decay<decltype(*oTypeData.begin())>::type;
 
-                if constexpr(std::is_convertible<O_T, I_T>::value) 
+                if constexpr (std::is_convertible<O_T, I_T>::value)
                 {
-                    iTypeData = std::vector<I_T> (oTypeData.begin(), oTypeData.end()); // No funciona con copy; 
-                } else  {
-                    static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value ,
+                    iTypeData = std::vector<I_T>(oTypeData.begin(), oTypeData.end()); // No funciona con copy;
+                }
+                else
+                {
+                    static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value,
                                 "A function needs to be implemented to transform OtoI and ItoO");
                     t(iTypeData, oTypeData);
                 }
-            } else  {
-                static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value ,
+            }
+            else
+            {
+                static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value,
                             "A function needs to be implemented to transform OtoI and ItoO");
                 t(iTypeData, oTypeData);
             }
             return true;
         };
-	    //bool clear(O & oTypeData){ oTypeData.clear() ; return true;};
+        //virtual bool clear(O & oTypeData)=0;
 };
 
-template <class I, class O, class C = ConverterDefault<I,O>> 
+template <class I, class O>
 class DoubleBuffer
 {
     private:
         mutable std::shared_mutex bufferMutex;
-        O bufferA, bufferB;
+        O bufferA; 
+        O bufferB;
         O &readBuffer = bufferA;
         O &writeBuffer = bufferB;
-        C *converter;
-        bool to_clear=false;
+        Converter<I, O> *converter; //Hay necesidad de que esto sea un puntero?
+        bool to_clear = false;
         std::atomic_bool empty = true;
-        
+
     public:
         DoubleBuffer()
         {
-            converter = new C();
+            converter = new Converter<I, O>();
         };
-        void init(C& _converter)
+
+        void init(Converter<I,O> &_converter)
         {
             converter = &_converter;
-            to_clear=false;
+            to_clear = false;
         }
         void clear()
         {
@@ -132,22 +134,20 @@ class DoubleBuffer
 
         std::optional<O> get()
         {
-            if(empty.load())
+            if (empty.load())
                 return {};
             std::shared_lock lock(bufferMutex);
             empty.store(true);
             return readBuffer;
         }
 
-        void put(const I &d, std::function<void(const I&, O&)> t = empty_fn)
+        void put(const I &d, std::function<void(const I &, O &)> t = empty_fn)
         {
             std::unique_lock lock(bufferMutex);
-            if( converter->ItoO(d, writeBuffer, t))
+            if (converter->ItoO(d, writeBuffer, t))
             {
                 std::swap(writeBuffer, readBuffer);
                 empty.store(false);
             }
         }
-    };
-
-
+};
