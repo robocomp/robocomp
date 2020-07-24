ccf5b925685acbbcdac757415407841a3ece4d89
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-07-24 18:23:31 +0200

    [tools/buildTools] Updated tools to use python3 interpreter.

--- tools/buildTools/rcbuild.py
+++ tools/buildTools/rcbuild.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
 from __future__ import print_function
 import argparse, argcomplete
--- tools/buildTools/rccomp.py
+++ tools/buildTools/rccomp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
 from __future__ import print_function
 import os
