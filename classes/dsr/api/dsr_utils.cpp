3ba5561325b01ce118e03a337c47b3f4477a2eca
Juan Carlos García García <juancarlos97gg@gmail.com>
2020-08-07 10:53:04 +0200

    Remove call to qCompress from write_file

--- classes/dsr/api/dsr_utils.cpp
+++ classes/dsr/api/dsr_utils.cpp
@@ -341,7 +341,7 @@ void Utilities::write_to_json_file(const std::string &json_file_path)
 	QJsonDocument jsonDoc = DSRGraph_to_QJsonDocument(G);
     QFile jsonFile(QString::fromStdString(json_file_path));
     jsonFile.open(QFile::WriteOnly);
-    jsonFile.write(qCompress(jsonDoc.toJson()));
+    jsonFile.write(/*qCompress(*/jsonDoc.toJson())/*)*/;
     jsonFile.close();
     auto now_c = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     qDebug() << __FILE__ << " " << __FUNCTION__ << "File: " << QString::fromStdString(json_file_path)<< " written to disk at " << now_c;
