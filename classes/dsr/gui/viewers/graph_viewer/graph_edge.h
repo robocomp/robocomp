be3265e2569b57fe8c2943ffd958606fcd696e30
rcintas <rcintas>
2020-10-19 13:18:02 +0200

    [DSR:graph_viewer] Update edge transform widget dinamically

--- classes/dsr/gui/viewers/graph_viewer/graph_edge.h
+++ classes/dsr/gui/viewers/graph_viewer/graph_edge.h
@@ -169,7 +169,8 @@ public:
             graph(graph_), from(from_), to(to_)
     {
         qRegisterMetaType<DSR::IDType>("DSR::IDType");
-
+        
+        connect(graph.get(), &DSR::DSRGraph::update_edge_signal, this, &DoRTStuff2::add_or_assign_edge_slot);
         //Inner Api
         inner_eigen = graph->get_inner_eigen_api();
         
@@ -215,6 +216,24 @@ public:
             show();
         }
     };
+    void generate_node_transform_list(std::string to, std::string from)
+    {
+        transform_set.clear();
+        transform_set.insert(from);
+        auto node = graph->get_node(from);
+        try{
+            std::optional<Node> parent_node;
+            do
+            {
+                parent_node = graph->get_parent_node(node.value());
+                if (parent_node.has_value())
+                {
+                    transform_set.insert(parent_node.value().name());
+                    node = parent_node;
+                }
+            }while(node.value().type() != "world" and node.value().name() != to);
+        }catch(...){}
+    };
     void closeEvent (QCloseEvent *event) override
     {
         disconnect(graph.get(), 0, this, 0);
@@ -222,10 +241,15 @@ public:
 public slots:
     void update_combo(QString combo_text)
     {
-        std::string reference = to_string;
+        this->reference = to_string;
         if (combo_text == "world")
-            reference = graph->get_node_root().value().name();
-        std::optional<Mat::Vector6d> transform = inner_eigen->transform_axis(reference, from_string);
+            this->reference = graph->get_node_root().value().name();
+        generate_node_transform_list(this->reference, this->from_string);
+        update_values();
+    };
+    void update_values()
+    {
+        std::optional<Mat::Vector6d> transform = inner_eigen->transform_axis(this->reference, this->from_string);
         if (transform.has_value())
         {
             for(unsigned int pos = 0;pos < 6;pos++)
@@ -237,15 +261,33 @@ public slots:
         else
             std::cout<<"Error retriving RT data"<<std::endl;
     };
-
+    void add_or_assign_edge_slot(const std::int32_t from, const std::int32_t to, const std::string& edge_type)
+    {
+        if(edge_type == "RT")
+        {
+            std::optional<Node> from_node = graph->get_node(from);
+            std::optional<Node> to_node = graph->get_node(to);
+            if(from_node.has_value() and to_node.has_value())
+            {
+                std::string from_str = from_node.value().name();
+                std::string to_str = to_node.value().name();
+                //check if any node is involved in actual reference transform
+                if(transform_set.find(from_str)!= transform_set.end() or transform_set.find(to_str) != transform_set.end())
+                    update_values();
+            }
+        }
+    };
+    
 private:
     std::shared_ptr<DSR::DSRGraph> graph;
     std::shared_ptr<DSR::InnerEigenAPI> inner_eigen;
     int from, to;
     std::string from_string;
     std::string to_string;
+    std::string reference;
     std::map<int, QLineEdit*> attrib_widgets;
     std::vector<std::vector<std::string>> attrib_names = {{" X", " Y", " Z"}, {"RX", "RY", "RZ"} }; 
+    std::set<std::string> transform_set;
 };
 
 class GraphEdge : public QObject, public QGraphicsLineItem, public std::enable_shared_from_this<GraphEdge>
