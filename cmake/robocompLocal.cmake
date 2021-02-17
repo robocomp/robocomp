1725d5e6c7d24c05b59ee9c87ba1e4c2dcdfab4e
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-02-17 07:39:47 +0100

    [cmake] OpenMP moved to the target that needs it. [cmake] Robocomp version updated. [cmake] Unneeded .ice generation avoided. [cmake] package target recovered. [cmake] rcmanagersimple cmakelists updated [cmake] rclogger cmakelists updated [cmake] Unifiying robocompLocal an robocomp cmake files

--- cmake/robocompLocal.cmake
+++ cmake/robocompLocal.cmake
@@ -1,12 +1,13 @@
 
 MACRO( ROBOCOMP_INITIALIZE )
-  set(RoboComp_VERSION 1.0 CACHE STRING "RoboComp version")
+  set(RoboComp_VERSION 2021.2.1 CACHE STRING "RoboComp version")
   #set install dirs
   set(BIN_INSTALL_DIR "/opt/robocomp-${RoboComp_VERSION}/bin")
   set(CONFIG_INSTALL_DIR "/opt/robocomp-${RoboComp_VERSION}/etc-default")
   # Set root directory
   SET( ROBOCOMP_ROOT ${ARGN} )
   MESSAGE(STATUS "RoboComp root is now set to ${ROBOCOMP_ROOT}")
+
   # Include path directories
   INCLUDE_DIRECTORIES (
     .
@@ -23,9 +24,9 @@ MACRO( ROBOCOMP_INITIALIZE )
 
   FIND_PACKAGE( Threads)
   FIND_PACKAGE( Ice REQUIRED COMPONENTS Ice IceStorm OPTIONAL_COMPONENTS IceUtil )
- 
+
   SET( LIBS ${LIBS} -L/opt/robocomp/lib ${OSG_LIBRARY} -losgViewer -losg -losgUtil  -losgGA ${OSGDB_LIBRARY} ${OSGVIEWER_LIBRARY} ${OPENTHREADS_LIBRARY}  -L${ROBOCOMP_ROOT}/classes ${CMAKE_THREAD_LIBS_INIT} -lboost_system  ${IPP_LIBS} )
- 
+
 ENDMACRO( ROBOCOMP_INITIALIZE )
 
 
@@ -47,15 +48,14 @@ ENDMACRO( ROBOCOMP_LIBRARY )
 MACRO( ROBOCOMP_WRAP_ICE )
   # External Slice source paths
   SET (EXTERNAL_SLICE "")
-  SET (SLICE_PATH "$ENV{SLICE_PATH};")
+  SET (SLICE_PATH "$ENV{SLICE_PATH};$ENV{ROBOCOMP}/interfaces;/opt/robocomp/interfaces;./ice_files/;")
   SET (INC_ROBOCOMPSLICE_PATH "true" )
   SET (ADDITIONAL_SLICE_INCLUDE_PATH "")
   FOREACH (SPATH ${SLICE_PATH})
-     #MESSAGE(STATUS "ACHO ${SLICE_PATH}")
      IF( ${RoboComp_INTERFACES_DIR} STREQUAL ${SPATH})
        SET(INC_ROBOCOMPSLICE_PATH "false")
      ELSE( ${RoboComp_INTERFACES_DIR} STREQUAL ${SPATH})
-			SET(ADDITIONAL_SLICE_INCLUDE_PATH ${ADDITIONAL_SLICE_INCLUDE_PATH} -I${SPATH})
+       SET(ADDITIONAL_SLICE_INCLUDE_PATH ${ADDITIONAL_SLICE_INCLUDE_PATH} -I${SPATH})
      ENDIF( ${RoboComp_INTERFACES_DIR} STREQUAL ${SPATH})
   ENDFOREACH (SPATH ${SLICE_PATH})
   IF (${INC_ROBOCOMPSLICE_PATH} STREQUAL "true")
@@ -78,12 +78,12 @@ MACRO( ROBOCOMP_WRAP_ICE )
         MESSAGE(STATUS "Adding rule to generate ${input_file}.cpp and ${input_file}.h from ${SPATH}/${input_file}.ice  (${SLICECPP_PATH}slice2cpp)" )
         ADD_CUSTOM_COMMAND (
           OUTPUT ${input_file}.cpp ${input_file}.h
-          COMMAND ${SLICECPP_PATH}slice2cpp -I${RoboComp_INTERFACES_DIR} ${ADDITIONAL_SLICE_INCLUDE_PATH} -I. ${SPATH}/${input_file}.ice --output-dir .
+          COMMAND ${SLICECPP_PATH}slice2cpp --underscore -I${RoboComp_INTERFACES_DIR} ${ADDITIONAL_SLICE_INCLUDE_PATH} -I. ${SPATH}/${input_file}.ice --output-dir .
           DEPENDS ${SPATH}/${input_file}.ice
           COMMENT "Generating ${input_file}.cpp and ${input_file}.h from ${input_file}.ice"
         )
         SET ( SOURCES ${SOURCES} ./${input_file}.cpp)
-	    SET_PROPERTY(SOURCE ${input_file}.cpp PROPERTY SKIP_AUTOGEN ON)
+        SET_PROPERTY(SOURCE ${input_file}.cpp PROPERTY SKIP_AUTOGEN ON)
         SET ( SLICE_FILE_FOUND "true")
 	break ()
       ENDIF (EXISTS "${SPATH}/${input_file}.ice")
@@ -96,6 +96,7 @@ ENDMACRO( ROBOCOMP_WRAP_ICE )
 
 
 MACRO( ROBOCOMP_IDSL_TO_ICE )
+  set(ROBOCOMP_ICE_TMP_PATH /tmp/)
   STRING (REPLACE "/" "_" SPECIFIC_TARGET "${CMAKE_CURRENT_SOURCE_DIR}") 
   ADD_CUSTOM_TARGET(ICES_${SPECIFIC_TARGET} ALL)
   SET( SPATH ${RoboComp_INTERFACES_DIR}/IDSLs/)
@@ -103,7 +104,7 @@ MACRO( ROBOCOMP_IDSL_TO_ICE )
       IF (EXISTS "${SPATH}/${input_file}.idsl")
         MESSAGE(STATUS "idsl=>ice: Adding rule to generate ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice from ${SPATH}/${input_file}.idsl")
         add_custom_command(
-          COMMAND "${CMAKE_HOME_DIRECTORY}/tools/robocompdsl/robocompdsl.py" ${SPATH}/${input_file}.idsl ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice
+          COMMAND "${CMAKE_HOME_DIRECTORY}/tools/robocompdsl/robocompdsl.py" ${SPATH}/${input_file}.idsl ${ROBOCOMP_ICE_TMP_PATH}/${input_file}.ice
           DEPENDS ${SPATH}/${input_file}.idsl
           COMMENT "idsl=>ice: Generating ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice from ${SPATH}/${input_file}.idsl"
           TARGET ICES_${SPECIFIC_TARGET}
@@ -121,7 +122,7 @@ MACRO( ROBOCOMP_ICE_TO_SRC )
     MESSAGE(STATUS "ice=>h/cpp: Adding rule to generate ${input_file}.h and ${input_file}.cpp from ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice")
     add_custom_command(
         OUTPUT ${input_file}.cpp ${input_file}.h
-        COMMAND slice2cpp ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice -I${CMAKE_HOME_DIRECTORY}/interfaces/ --output-dir .
+        COMMAND slice2cpp ${ROBOCOMP_ICE_TMP_PATH}/${input_file}.ice -I${CMAKE_HOME_DIRECTORY}/interfaces/ --output-dir .
         DEPENDS ICES_${SPECIFIC_TARGET}
         COMMENT "ice=>h/cpp: Generating ${input_file}.h and ${input_file}.cpp from ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice"
     )
@@ -137,7 +138,7 @@ function( ROBOCOMP_ICE_TO_SRC_FUNC result )
     MESSAGE(STATUS "ice=>h/cpp: Adding rule to generate ${input_file}.h and ${input_file}.cpp from ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice")
     add_custom_command(
             OUTPUT ${input_file}.cpp ${input_file}.h
-            COMMAND slice2cpp ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice -I${CMAKE_HOME_DIRECTORY}/interfaces/ --output-dir .
+            COMMAND slice2cpp ${ROBOCOMP_ICE_TMP_PATH}/${input_file}.ice -I${CMAKE_HOME_DIRECTORY}/interfaces/ --output-dir .
             DEPENDS ICES_${SPECIFIC_TARGET}
             COMMENT "ice=>h/cpp: Generating ${input_file}.h and ${input_file}.cpp from ${CMAKE_HOME_DIRECTORY}/interfaces/${input_file}.ice"
     )
@@ -148,12 +149,13 @@ function( ROBOCOMP_ICE_TO_SRC_FUNC result )
 endfunction( ROBOCOMP_ICE_TO_SRC_FUNC )
 
 
-INCLUDE_DIRECTORIES (
-  ${CMAKE_CURRENT_BINARY_DIR}
-  .
-  ${ROBOCOMP_ROOT}/classes/
-  ${CMAKE_BINARY_DIR}
-)
+#INCLUDE_DIRECTORIES (
+#  ${CMAKE_CURRENT_BINARY_DIR}
+#  .
+#  /opt/robocomp/include/
+#  ${ROBOCOMP_ROOT}/classes/
+#  ${CMAKE_BINARY_DIR}
+#)
 
 
 MACRO( ROBOCOMP_WRAP_PYTHON_UI )
