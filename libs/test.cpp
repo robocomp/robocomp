ebba4fba85d797ad576991887415f1b712e22d74
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-03-30 16:53:17 +0200

    More testing

--- libs/test.cpp
+++ libs/test.cpp
@@ -3,10 +3,13 @@
 //
 
 #include <innermodel/innermodel.h>
+#include <osgviewer/osgview.h>
 
 int main()
 {
     InnerModel* innerModel = new InnerModel();
     delete innerModel;
+    OsgView* osg_view = new OsgView();
+    delete osg_view;
     return 0;
 }
