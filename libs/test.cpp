9d1265f72b41826a60ebd7a214a2f641a74bd4dd
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-03-30 13:20:07 +0200

    added test.cpp to test libs build

---
+++ libs/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by robolab on 30/3/21.
+//
+
+#include <innermodel/innermodel.h>
+
+int main()
+{
+    InnerModel* innerModel = new InnerModel();
+    delete innerModel;
+    return 0;
+}
