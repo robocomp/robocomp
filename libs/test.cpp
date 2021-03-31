bbec61a76385434a393cea6cfbd806dc62230957
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-03-31 11:36:27 +0200

    test compiling

--- libs/test.cpp
+++ libs/test.cpp
@@ -4,12 +4,20 @@
 
 #include <innermodel/innermodel.h>
 #include <osgviewer/osgview.h>
+#include <qapplication.h>
 
-int main()
+
+
+int main( int argc, char **argv )
 {
+    QApplication a( argc, argv );
+
     InnerModel* innerModel = new InnerModel();
     delete innerModel;
+
     OsgView* osg_view = new OsgView();
+    osg_view->show();
+    int rvalue = a.exec();
     delete osg_view;
-    return 0;
+    return rvalue;
 }
