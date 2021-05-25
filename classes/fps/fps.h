ed1da30756e50df34bef2ab1faedd2773d4baccf
pbustos <pbustos@unex.es>
2021-05-25 20:36:10 +0200

    bug

--- classes/fps/fps.h
+++ classes/fps/fps.h
@@ -5,7 +5,20 @@ class FPSCounter
 		{
 			begin = std::chrono::high_resolution_clock::now();
 		}
-		int print( const std::string &text, const unsigned int msPeriod = 1000)
+        int print( const std::string &text, const unsigned int msPeriod = 1000)
+        {
+            auto end = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration<double>(end - begin).count() * 1000;
+            if( elapsed > msPeriod)
+            {
+                std::cout << "Epoch time = " << elapsed/cont << "ms. Fps = " << cont << " " << text << std::endl;
+                begin = std::chrono::high_resolution_clock::now();
+                cont = 0;
+            }
+            cont++;
+            return elapsed;
+        }
+		void print( const std::string &text, std::function<void(int)> f, const unsigned int msPeriod = 1000)
 		{	
 			auto end = std::chrono::high_resolution_clock::now();
 			auto elapsed = std::chrono::duration<double>(end - begin).count() * 1000;
@@ -13,10 +26,10 @@ class FPSCounter
 			{
 				std::cout << "Epoch time = " << elapsed/cont << "ms. Fps = " << cont << " " << text << std::endl;
 				begin = std::chrono::high_resolution_clock::now();
+				f(cont);
 				cont = 0;
 			}
 			cont++;
-			return cont;
 		}
 		std::chrono::time_point<std::chrono::high_resolution_clock> begin;
 		int cont = 0;
