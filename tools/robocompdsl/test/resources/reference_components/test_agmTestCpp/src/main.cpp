2c3bc0b61c931dfdedfcfecc24319044673efbbc
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-06-09 17:32:15 +0200

    [tools/robocompdsl] Minor Qt problem fix. Minor qt define problem.

--- tools/robocompdsl/test/resources/reference_components/test_agmTestCpp/src/main.cpp
+++ tools/robocompdsl/test/resources/reference_components/test_agmTestCpp/src/main.cpp
@@ -63,7 +63,7 @@
 
 // QT includes
 #include <QtCore>
-#include <QtGui>
+#include <QtWidgets>
 
 // ICE includes
 #include <Ice/Ice.h>
@@ -329,43 +329,35 @@ int main(int argc, char* argv[])
 	QString prefix("");
 	if (argc > 1)
 	{
-	    QString initIC = QString("--Ice.Config=");
-	    for (int i = 1; i < argc; ++i)
-		{
-		    arg = argv[i];
-            if (arg.find(initIC.toStdString(), 0) == 0)
-            {
-                configFile = QString::fromStdString(arg).remove(0, initIC.size());
-            }
-            else
-            {
-                configFile = QString::fromStdString(argv[1]);
-            }
-        }
 
-        // Search in argument list for --prefix= argument (if exist)
-        QString prfx = QString("--prefix=");
-        for (int i = 2; i < argc; ++i)
-        {
-            arg = argv[i];
-            if (arg.find(prfx.toStdString(), 0) == 0)
-            {
-                prefix = QString::fromStdString(arg).remove(0, prfx.size());
-                if (prefix.size()>0)
-                    prefix += QString(".");
-                printf("Configuration prefix: <%s>\n", prefix.toStdString().c_str());
-            }
-        }
-
-        // Search in argument list for --test argument (if exist)
-        QString startup = QString("--startup-check");
+		// Search in argument list for arguments
+		QString startup = QString("--startup-check");
+		QString initIC = QString("--Ice.Config=");
+		QString prfx = QString("--prefix=");
 		for (int i = 0; i < argc; ++i)
 		{
 			arg = argv[i];
-			if (arg.find(startup.toStdString(), 0) == 0)
+			if (arg.find(startup.toStdString(), 0) != std::string::npos)
 			{
 				startup_check_flag = true;
 				cout << "Startup check = True"<< endl;
+			}
+			else if (arg.find(prfx.toStdString(), 0) != std::string::npos)
+			{
+				prefix = QString::fromStdString(arg).remove(0, prfx.size());
+				if (prefix.size()>0)
+					prefix += QString(".");
+				printf("Configuration prefix: <%s>\n", prefix.toStdString().c_str());
+			}
+			else if (arg.find(initIC.toStdString(), 0) != std::string::npos)
+			{
+				configFile = QString::fromStdString(arg).remove(0, initIC.size());
+				qDebug()<<__LINE__<<"Starting with config file:"<<configFile;
+			}
+			else if (i==1 and argc==2 and arg.find("--", 0) == std::string::npos)
+			{
+				configFile = QString::fromStdString(arg);
+				qDebug()<<__LINE__<<QString::fromStdString(arg)<<argc<<arg.find("--", 0)<<"Starting with config file:"<<configFile;
 			}
 		}
 
--- tools/robocompdsl/test/resources/reference_components/test_implementsPython/src/genericworker.py
+++ tools/robocompdsl/test/resources/reference_components/test_implementsPython/src/genericworker.py
@@ -35,58 +35,11 @@ import RoboCompCommonBehavior
 
 class GenericWorker():
 
-    #Signals for State Machine
-    t_initialize_to_compute = QtCore.Signal()
-    t_compute_to_compute = QtCore.Signal()
-    t_compute_to_finalize = QtCore.Signal()
-
-    #-------------------------
 
     def __init__(self, mprx):
         super(GenericWorker, self).__init__()
 
 
-        #State Machine
-        self.defaultMachine= QtCore.QStateMachine()
-        self.compute_state = QtCore.QState(self.defaultMachine)
-        self.initialize_state = QtCore.QState(self.defaultMachine)
 
-        self.finalize_state = QtCore.QFinalState(self.defaultMachine)
-
-
-        #------------------
-        #Initialization State machine
-        self.initialize_state.addTransition(self.t_initialize_to_compute, self.compute_state)
-        self.compute_state.addTransition(self.t_compute_to_compute, self.compute_state)
-        self.compute_state.addTransition(self.t_compute_to_finalize, self.finalize_state)
-
-
-        self.compute_state.entered.connect(self.sm_compute)
-        self.initialize_state.entered.connect(self.sm_initialize)
-        self.finalize_state.entered.connect(self.sm_finalize)
-        self.timer.timeout.connect(self.t_compute_to_compute)
-
-        self.defaultMachine.setInitialState(self.initialize_state)
-
-        #------------------
-
-    #Slots funtion State Machine
-
-    @QtCore.Slot()
-    def sm_compute(self):
-        print("Error: lack sm_compute in Specificworker")
-        sys.exit(-1)
-
-    @QtCore.Slot()
-    def sm_initialize(self):
-        print("Error: lack sm_initialize in Specificworker")
-        sys.exit(-1)
-
-    @QtCore.Slot()
-    def sm_finalize(self):
-        print("Error: lack sm_finalize in Specificworker")
-        sys.exit(-1)
-
-    #-------------------------
 
 
