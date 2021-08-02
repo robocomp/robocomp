3fcdb27b4c1a1dcc0c29595279edcb78c9f61923
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-08-02 07:23:13 +0200

    rcportchecker moved and created setup.py

---
+++ tools/rcportchecker/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rcportchecker',
+    version='0.0.1',
+    packages=['rcportchecker', 'rcportchecker.rcportchecker'],
+    url='https://github.com/robocomp/robocomp/tree/development/tools/rcportchecker',
+    license='GPL V3',
+    author='Esteban Martinena',
+    author_email='emartinena@unex.es',
+    description='Port checker of robocomp components'
+)
