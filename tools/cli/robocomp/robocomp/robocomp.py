a94cc5b462ec9b9ee81f77694796541d1f5c4596
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-08-18 08:08:30 +0200

    [tools] Minor change in robocomp cli

--- tools/cli/robocomp/robocomp/robocomp.py
+++ tools/cli/robocomp/robocomp/robocomp.py
@@ -5,8 +5,8 @@ from rcconfig import rcconfig
 from rcdocker import rcdocker
 from rcportchecker import rcportchecker
 from rcrun import rcrun
-from rcworkspace import rcworkspace
-from robocompdsl import main
+from rcworkspace import main as rcworkspace
+from robocompdsl import main as robocompdsl
 
 
 app = typer.Typer()
@@ -17,7 +17,7 @@ app.add_typer(rcportchecker.app, name="list")
 app.add_typer(rcrun.app, name="run")
 app.add_typer(rcworkspace.app, name="workspace")
 app.command(name="cd")(rccd.cd_exec)
-app.command(name="dsl")(main.generate)
+app.command(name="dsl")(robocompdsl.generate)
 
 
 if __name__ == "__main__":
