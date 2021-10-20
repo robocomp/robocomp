a20e74ac2473552979c1753e0e5cb248e4ba8213
Esteban Martinena Guerrero <martinena@univention.de>
2021-10-20 17:37:33 +0200

    rcbuild_and_robocompdsl_testing

--- tools/cli/rcbuild/setup.py
+++ tools/cli/rcbuild/setup.py
@@ -5,6 +5,10 @@ setup(
     version='0.0.1',
     include_package_data=True,
     packages=find_packages(),
+	install_requires=[
+		'python-apt',
+		'docker'
+	],
     url='https://github.com/robocomp/robocomp/tree/development/tools/cli/rcbuild',
     license='GPL V3',
     author='Esteban Martinena',
