a5ebeb279073f88b4c080f31d30c757b107531bd
Esteban Martinena Guerrero <martinena@univention.de>
2021-10-31 18:00:50 +0100

    Fixed Docker import

--- tools/cli/robocomp/robocomp/robocomp.py
+++ tools/cli/robocomp/robocomp/robocomp.py
@@ -11,7 +11,6 @@ from robocompdsl import main as robocompdsl
 app = typer.Typer(help="Tool to manage and group all the Robocomp commands.")
 app.add_typer(rcbuild.app, name="build")
 app.add_typer(rcconfig.app, name="config")
-
 app.add_typer(rcportchecker.app, name="list")
 app.add_typer(rcrun.app, name="run")
 app.add_typer(rcworkspace.app, name="workspace")
@@ -21,8 +20,8 @@ app.command(name="dsl", help=typer.style("Tool to create components and interfac
 try:
     from rcdocker import rcdocker
     app.add_typer(rcdocker.app, name="docker")
-except FileNotFoundError:
-    print("Docker not found. Docker commands will not be available.")
+except Exception as e:
+    print("Docker not found. Docker commands will not be available.", e)
 
 if __name__ == "__main__":
     app()
