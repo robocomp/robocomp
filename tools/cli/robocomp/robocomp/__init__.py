a0a9c84f572cec77f818f6cd9d06bd2b5580d315
Esteban Martinena Guerrero <martinena@univention.de>
2021-11-08 09:05:21 +0100

    change_parent_process_directory refactored to execute_command_in_current_shell and moved to robocomp.

--- tools/cli/robocomp/robocomp/__init__.py
+++ tools/cli/robocomp/robocomp/__init__.py
@@ -1,3 +1,4 @@
+import os
 import shlex
 import subprocess
 import sys
@@ -22,6 +23,29 @@ def execute_command(command, dry_command=False):
     else:
         typer.secho(f"FAILED with {output} and return_code = {return_code}", fg=typer.colors.RED)
     print("=============================")
+
+def execute_command_in_current_shell(command, dry_command=False):
+    # Horrible hack to be able to change directory in the current terminal session
+    def quote_against_shell_expansion(s: str):
+        import pipes
+        return pipes.quote(s)
+
+    def put_text_back_into_terminal_input_buffer(text: str):
+        if os.isatty(sys.stdout.fileno()):
+            import fcntl
+            import termios
+            try:
+                # (and if the user types while it runs they could insert characters between the characters in 'text'!)
+                for c in text:
+                    fcntl.ioctl(1, termios.TIOCSTI, c)
+            except OSError as e:
+                print(f"Problem finding interactive terminal to execute cd.\n>>{e}")
+        else:
+            print(f"Problem finding interactive terminal to execute cd.")
+
+    args = " ".join([quote_against_shell_expansion(argument) for argument in shlex.split(command)[1:]])
+    # the horror
+    put_text_back_into_terminal_input_buffer(f"{command} {args} \n")
 
 def is_interactive():
     return sys.__stdin__.isatty()
