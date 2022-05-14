5ecd8ccf8c73fcdf180c692ca7911e273c6171c3
pbustos <pbustos@unex.es>
2022-05-14 23:04:33 +0200

    fps

--- classes/fps/fps.h
+++ classes/fps/fps.h
@@ -7,16 +7,18 @@ class FPSCounter
 		}
         int print( const std::string &text, const unsigned int msPeriod = 1000)
         {
+            static int fps=0;
             auto end = std::chrono::high_resolution_clock::now();
             auto elapsed = std::chrono::duration<double>(end - begin).count() * 1000;
             if( elapsed > msPeriod)
             {
                 std::cout << "Epoch time = " << elapsed/cont << "ms. Fps = " << cont << " " << text << std::endl;
                 begin = std::chrono::high_resolution_clock::now();
+                fps=cont;
                 cont = 0;
             }
             cont++;
-            return elapsed;
+            return fps;
         }
 		void print( const std::string &text, std::function<void(int)> f, const unsigned int msPeriod = 1000)
 		{	
