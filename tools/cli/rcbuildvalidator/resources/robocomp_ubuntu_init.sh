6b3365676e6306b421aaed0360739e0e44e1aefc
Esteban Martinena <orensbruli@gmail.com>
2022-11-13 09:55:36 +0100

    Removing tools directory
    Moved to external repo https://github.com/robocomp/robocomp_tools

--- tools/cli/rcbuildvalidator/resources/robocomp_ubuntu_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get --yes install \
-    vim \
-    sudo
-useradd -m -s /bin/bash robolab
-usermod -aG sudo robolab
-echo "robolab  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
