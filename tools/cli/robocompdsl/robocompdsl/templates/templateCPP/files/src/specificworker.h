7e0e8fe8cddf93deb6fed5151d0c5fa9785f1ef6
Esteban Martinena Guerrero <orensbruli@gmail.com>
2023-11-28 10:08:11 +0100

    Remove innermodel includes

--- tools/cli/robocompdsl/robocompdsl/templates/templateCPP/files/src/specificworker.h
+++ tools/cli/robocompdsl/robocompdsl/templates/templateCPP/files/src/specificworker.h
@@ -29,8 +29,6 @@ ${agmagent_comment}
 #define SPECIFICWORKER_H
 
 #include <genericworker.h>
-${innermodel_include}
-${innermodelviewer_includes}
 ${dsr_includes}
 
 class SpecificWorker : public GenericWorker
